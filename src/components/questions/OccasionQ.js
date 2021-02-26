e3034f99105772cd2dc9ab08f59b831ebabe7c5f
Ravi Campos <rnscampos@gmail.com>
2021-02-26 17:03:27 -0300

    page passage bug fixed

--- src/components/questions/OccasionQ.js
+++ src/components/questions/OccasionQ.js
@@ -2,12 +2,12 @@ import { useState, useEffect } from "react";
 
 function OccasionQ({ tools: { setPage, setOccasion, occasion, section }}) {
 
-    // const getSelectedRadio = () => {
-    //     let radios = document.querySelectorAll("input[name='occasion']");
-    //     radios = [...radios];
-    //     radios = radios.filter(radio => radio.checked)
-    //     return radios[0].value;
-    // }
+    const getSelectedRadio = () => {
+        let radios = document.querySelectorAll("input[name='occasion']");
+        radios = [...radios];
+        radios = radios.filter(radio => radio.checked)
+        return JSON.parse(radios[0].value);
+    }
 
     const [ radioValue, setRadioValue ] = useState({occasion, section});
 
@@ -22,19 +22,19 @@ function OccasionQ({ tools: { setPage, setOccasion, occasion, section }}) {
 
             <div>
                 <div>
-                    <input type="radio" name="occasion" id="occ_bday" value='{"occasion":"aniversário do presenteado", "section": "bday"}' checked={radioValue.occasion === "aniversário do presenteado"} onChange={e=> setRadioValue(JSON.parse(e.target.value)) }/>
+                    <input type="radio" name="occasion" id="occ_bday" value='{"occasion":"aniversário do presenteado", "section": "bday"}' checked={radioValue.occasion === "aniversário do presenteado"} onChange={e=> setRadioValue(JSON.parse(e.target.value))}/>
                     <label htmlFor="occ_bday">Sim, é aniversário desta pessoa!</label>
                 </div>
-                <div>
+                {/* <div>
                     <input type="radio" name="occasion" id="occ_wedding" value='{"occasion":"aniversário de namoro/casamento", "section":"daiting bday"}' checked={radioValue.occasion === "aniversário de namoro/casamento"} onChange={e=> setRadioValue(JSON.parse(e.target.value)) }/>
                     <label htmlFor="occ_wedding">Sim, é aniversário de namoro/casamento</label>
-                </div>
+                </div> */}
                 <div>
-                    <input type="radio" name="occasion" id="occ_offspring" value='{"occasion":"vai ter um filho", "section": "new offspring"}' checked={radioValue.occasion === "vai ter um filho"} onChange={e=> setRadioValue(JSON.parse(e.target.value)) }/>
+                    <input type="radio" name="occasion" id="occ_offspring" value='{"occasion":"vai ter um filho", "section": "new offspring"}' checked={radioValue.occasion === "vai ter um filho"} onChange={e=> setRadioValue(JSON.parse(e.target.value))}/>
                     <label htmlFor="occ_offspring">Sim, acabamos de descobrir que essa pessoa vai ter um filho</label>
                 </div>
                 <div>
-                    <input type="radio" name="occasion" id="occ_promotion" value='{"occasion": "promoção no trabalho", "section": "promotion"}' checked={radioValue.occasion === "promoção no trabalho"} onChange={e=> setRadioValue(JSON.parse(e.target.value)) }/>
+                    <input type="radio" name="occasion" id="occ_promotion" value='{"occasion": "promoção no trabalho", "section": "promotion"}' checked={radioValue.occasion === "promoção no trabalho"} onChange={e=> setRadioValue(JSON.parse(e.target.value))}/>
                     <label htmlFor="occ_promotion">Sim, essa pessoa acabou de ser promovida no trabalho</label>
                 </div>
                 <div>
@@ -42,18 +42,20 @@ function OccasionQ({ tools: { setPage, setOccasion, occasion, section }}) {
                     <label htmlFor="occ_house">Sim, essa pessoa está se mudando para uma casa nova!</label>
                 </div>
                 <div>
-                    <input type="radio" name="occasion" id="occ_nop" value='{"occasion": "não preciso de ocasiões", "section": "no occasion"}' checked={radioValue.occasion === "não preciso de ocasiões"} onChange={e=> setRadioValue(JSON.parse(e.target.value)) }/>
+                    <input type="radio" name="occasion" id="occ_nop" value='{"occasion": "não preciso de ocasiões", "section": "no occasion"}' checked={radioValue.occasion === "não preciso de ocasiões"} onChange={e=> setRadioValue(JSON.parse(e.target.value))}/>
                     <label htmlFor="occ_nop">Claro que não, não preciso de ocasiões especiais para dar presentes!</label>
                 </div>
             </div>
 
             <button onClick={() => {
-                setOccasion(radioValue.occasion);
+                const radioSelected = getSelectedRadio();
+                setOccasion(radioSelected.occasion);
                 setPage({section: "inicio", index: 1})
             }}>Anterior</button>
             <button onClick={() => {
-                setOccasion(radioValue.occasion);
-                setPage({section: radioValue.section, index: 0})
+                const radioSelected = getSelectedRadio();
+                setOccasion(radioSelected.occasion);
+                setPage({section: radioSelected.section, index: 0})
             }}>Próxima</button>
         </div>
     )
