d2164991c910655f6365913f480c81954703aa33
Ravi Campos <rnscampos@gmail.com>
2021-03-16 20:33:20 -0300

    intimacy bar added

--- src/components/pages/questions/bday/individual-questions/IntimacyQ.js
+++ src/components/pages/questions/bday/individual-questions/IntimacyQ.js
@@ -6,7 +6,16 @@ function IntimacyQ({tools: {intimacy, setIntimacy, setBDayPage}}) {
     
     return (
         <div>
-            <h2>Quão prómimos vocês são?</h2>
+            <h2>Agora me conta uma coisa, só entre a gente, o quão bem você conhece (GIFTED NAME)</h2>
+
+            <div id="slider-break-house" class="second-house">
+                <div class="shade"></div>
+                <div id="slider-break" class="pointer">
+                    <div id="slider-break-bar"></div>
+                    <div id="slider-break-ball"></div>
+                </div>
+            </div>
+            
             <br/>
 
             <button onClick={() => {
