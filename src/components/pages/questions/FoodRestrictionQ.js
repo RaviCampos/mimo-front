e5bfee38a33fea28f6f050661bce920b0fd7d7a5
Ravi Campos <rnscampos@gmail.com>
2021-03-25 19:58:56 -0300

    validation in FoodRestrictionQ

--- src/components/pages/questions/FoodRestrictionQ.js
+++ src/components/pages/questions/FoodRestrictionQ.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function FoodRestrictionQ({tools: {setPage, setFoodRestriction, foodRestriction, giftedName}}) {
 
@@ -7,13 +7,20 @@ function FoodRestrictionQ({tools: {setPage, setFoodRestriction, foodRestriction,
     if(foodRestriction) {
         preRestriction = foodRestriction.split(": ")
     } else {
-        preRestriction = ["não que eu saiba"]
+        preRestriction = ["", ""]
     }
 
     const [ inRestriction, setInRestriction ] = useState(preRestriction[0])
+    const [ complement, setComplement ] = useState(preRestriction[1])
 
-    const [ complement, setComplement ] = useState(preRestriction[1] ? preRestriction[1] : "")
+    const [ warning, setWarning ] = useState(false)
 
+    useEffect(() => {
+        setWarning(false);
+    }, [inRestriction])
+    useEffect(() => {
+        setWarning(false)
+    }, [complement])
     return (
         <div>
             <h2>Falar algo como: A gente as vezes gosta de dar gostosuras comestíveis nos nossos presentes, mas a gente sabe que as comidinhas não funcionam para todas as ocasiões</h2>
@@ -29,14 +36,19 @@ function FoodRestrictionQ({tools: {setPage, setFoodRestriction, foodRestriction,
                 </div>
                 <div>
                     <input type="radio" name="food" id="food_alergy" value="tem alergia ou não come de jeito nenhum" checked={inRestriction === "tem alergia ou não come de jeito nenhum"} onChange={e=> { setInRestriction(e.target.value); setComplement("")}}/>
-                    <label htmlFor="food_alergy">Sim, {giftedName} tem alergia ou não come de jeito nenhum (espaço para escrever)</label>
+                    <label htmlFor="food_alergy">Sim, {giftedName} tem alergia ou não come de jeito nenhum</label>
                     {inRestriction === "tem alergia ou não come de jeito nenhum" && <input type="text" value={complement} onChange={e => setComplement(e.target.value)}/>}
                 </div>
                 <div>
                     <input type="radio" name="food" id="food_dontwant" value="Acho que comida de presente não combina muito com a ocasião e/ou a pessoa" checked={inRestriction === "Acho que comida de presente não combina muito com a ocasião e/ou a pessoa"} onChange={e=> { setInRestriction(e.target.value); setComplement("")}}/>
                     <label htmlFor="food_dontwant">Não acho que vem ao caso. Acho que comida de presente não combina muito com a ocasião e/ou a pessoa</label>
                 </div>
+
+                {warning && <p className="validation-warning">{warning}</p>}
+
             </div>
+
+            <button onClick={() => setInRestriction("")}>Clean</button>
             <button onClick={() => {
                 if(complement) {
                     const answer = `${inRestriction}: ${complement}`
@@ -47,13 +59,19 @@ function FoodRestrictionQ({tools: {setPage, setFoodRestriction, foodRestriction,
                 setPage(4);
             }}>Anterior</button>
             <button onClick={() => {
-                if(complement) {
-                    const answer = `${inRestriction}: ${complement}`
-                    setFoodRestriction(answer);
+                if(!inRestriction) {
+                    setWarning("Por favor, selecione alguma das opções")
+                } else if(inRestriction === "tem alergia ou não come de jeito nenhum" && !complement) {
+                    setWarning(`Por favor, preencha que alimento(s) ${giftedName} não pode comer`)
                 } else {
-                    setFoodRestriction(inRestriction);
+                    if(complement) {
+                        const answer = `${inRestriction}: ${complement}`
+                        setFoodRestriction(answer);
+                    } else {
+                        setFoodRestriction(inRestriction);
+                    }
+                    setPage(6);
                 }
-                setPage(6);
             }}>Próxima</button>
         </div>
     )
