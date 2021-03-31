478117986aaa42d268d3d73d954a8c4181e2ddea
Ravi Campos <rnscampos@gmail.com>
2021-03-31 12:38:07 -0300

    giftednameq  with validation

--- src/components/pages/questions/work/individual-questions/GiftedNameQ.js
+++ src/components/pages/questions/work/individual-questions/GiftedNameQ.js
@@ -1,7 +1,48 @@
-function GiftedNameQ() {
+import { useEffect, useState } from "react"
+
+function GiftedNameQ({tools: { setSection, futureWork, setWork, setPage, setGoToOccasionLastQ, setWorkPage, giftedName, setGiftedName }}) {
+
+    const [ inName, setInName ] = useState(giftedName ? giftedName : "")
+
+    const [ warning, setWarning] = useState(false)    
+    useEffect(() => {
+        setWarning(false)
+    }, [inName])
+    
     return (
         <div>
-            <h2>Heloo from work</h2>
+            <h2>Comemorar conquistas é sempre muito importante! Qual é o nome da pessoa que vai receber o presente?</h2>
+            <input type="text" value={inName} onChange={e => setInName(e.target.value)}/>
+
+            <br/>
+
+            {warning && <p className="validation-warning">{warning}</p>}
+            
+            <button onClick={() => {
+                // const isInCouple =  inCouple ? "Sim" : "Não"
+                const work = {
+                    ...futureWork,
+                    giftedName: inName
+                }
+                // if(inCouple) {
+                //     delete wed.reasonToGift
+                // } else {
+                //     delete wed.commonHobbies
+                // }
+                setWork(work);
+                setGoToOccasionLastQ(false)
+                setSection("common")
+                setPage(2);
+            }}>Anterior</button>
+            <button onClick={() => {
+                if(inName !== "") {
+                    setGiftedName(inName);
+                    setWorkPage(1)
+                } else {
+                    setWarning("Por favor, preencha o nome da pessoa que vai ganhar o presente")
+                }
+            }}>Próxima</button>
+            
         </div>
     )
 }
