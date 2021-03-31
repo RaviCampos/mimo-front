617835420e8d4d76bf14d9681ef70c59e5c688ac
Ravi Campos <rnscampos@gmail.com>
2021-03-31 12:52:25 -0300

    reasonq plugged

--- src/components/pages/questions/work/Work.js
+++ src/components/pages/questions/work/Work.js
@@ -2,15 +2,18 @@ import { useState } from 'react';
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import NotFoundQ from "../NotFoundQ"
 import GiftedNameQ from "./individual-questions/GiftedNameQ"
+import ReasonQ from "./individual-questions/ReasonQ"
 
-function Work({tools: { setSection, work, setWork, setPage, goToOccasionLastQ, setGoToOccasionLastQ }}) {
+function Work({tools: { setSection, work, setWork, setPage, goToOccasionLastQ, setGoToOccasionLastQ, gifterName }}) {
 
     const [ workPage, setWorkPage ] = useState(goToOccasionLastQ ? 7 : 0); /* integer */
 
     const [ giftedName, setGiftedName ] = useState(work.giftedName)
+    const [ reason, setReason ] = useState(work.reason)
 
     const futureWork = {
-        giftedName
+        giftedName,
+        reason
     }
 
     let Question, tools
@@ -19,6 +22,10 @@ function Work({tools: { setSection, work, setWork, setPage, goToOccasionLastQ, s
         case 0:
             Question = GiftedNameQ;
             tools = { setSection, futureWork, setWork, setPage, setGoToOccasionLastQ, setWorkPage, giftedName, setGiftedName };
+            break
+        case 1:
+            Question = ReasonQ;
+            tools = { setWorkPage, reason, setReason, giftedName };
             break
         default:
             Question = NotFoundQ;
---
+++ src/components/pages/questions/work/individual-questions/ReasonQ.js
@@ -0,0 +1,10 @@
+
+function ReasonQ({tools: { setWorkPage, reason, setReason, giftedName }}) {
+    return (
+        <div>
+            <h2>Me conta, em qual circunstância vamos entregar o presente para {giftedName}</h2>
+        </div>
+    )
+}
+
+export default ReasonQ
