38d5c9375fcc79f4234926ff100aae0fec3b00cf
Ravi Campos <rnscampos@gmail.com>
2021-04-01 19:29:14 -0300

    reasonq in moving section with validation

--- src/components/pages/questions/moving/individual-questions/ReasonQ.js
+++ src/components/pages/questions/moving/individual-questions/ReasonQ.js
@@ -1,128 +1,60 @@
 import { useEffect, useState } from "react"
 
-function ReasonQ({tools: { setWorkPage, reason, setReason, giftedName }}) {
+function ReasonQ({tools: { setMovingPage, reason, setReason, giftedName }}) {
 
-    let protoReason
-
-    if(reason) {
-        protoReason = reason.split(" -- ")
-        if(protoReason.length === 1) {
-            protoReason.push("")
-        }
-    } else {
-        protoReason = [ "", "" ]
-    }
-
-    const [ inReason, setInReason ] = useState(protoReason[0])
-    const [ complement, setComplement ] = useState(protoReason[1])
+    const [ inReason, setInReason ] = useState(reason ? reason : "")
 
     const [ warning, setWarning ] = useState(false)
     useEffect(() => {
         setWarning(false)
-    }, [inReason, complement])
-    
-    function changeInReason(input) {
-        setInReason(input);
-        setComplement("")
-    } 
+    }, [inReason])
 
     return (
         <div>
-            <h2>Me conta, em qual circunstância vamos entregar o presente para {giftedName}</h2>
+            <h2>Me conta, em qual é a circunstância vamos entregar o presente para {giftedName}?</h2>
             <div>
-                <input type="radio" name="reason" id="reason_firstjob" checked={inReason === "Primeiro emprego"} onChange={() => changeInReason("Primeiro emprego")} />
-                <label htmlFor="reason_firstjob">Primeiro emprego</label>
+                <input type="radio" name="reason" id="reason_first-leaving" checked={inReason === `${giftedName} está saindo de casa pela primeira vez`} onChange={() => setInReason(`${giftedName} está saindo de casa pela primeira vez`)} />
+                <label htmlFor="reason_first-leaving">{giftedName} está saindo de casa pela primeira vez</label>
             </div>
             <div>
-                <input type="radio" name="reason" id="reason_newjob" checked={inReason === "Conseguiu um emprego que queria ou precisava muito"} onChange={() => changeInReason("Conseguiu um emprego que queria ou precisava muito")} />
-                <label htmlFor="reason_newjob">Conseguiu um emprego que queria ou precisava muito</label>
+                <input type="radio" name="reason" id="reason_living-out" checked={inReason === `${giftedName} vai morar fora`} onChange={() => setInReason(`${giftedName} vai morar fora`)} />
+                <label htmlFor="reason_living-out">{giftedName} vai morar fora</label>
             </div>
             <div>
-                <input type="radio" name="reason" id="reason_promotionjob" checked={inReason === "Foi promovido no emprego"} onChange={() => changeInReason("Foi promovido no emprego")} />
-                <label htmlFor="reason_promotionjob">Foi promovido no emprego</label>
+                <input type="radio" name="reason" id="reason_living-partner" checked={inReason === `${giftedName} vai morar com o(a) namorado(a)`} onChange={() => setInReason(`${giftedName} vai morar com o(a) namorado(a)`)} />
+                <label htmlFor="reason_living-partner">{giftedName} vai morar com o(a) namorado(a)</label>
             </div>
             <div>
-                <input type="radio" name="reason" id="reason_changejob" checked={inReason === "Mudando de emprego"} onChange={() => changeInReason("Mudando de emprego")} />
-                <label htmlFor="reason_changejob">Mudando de emprego</label>
+                <input type="radio" name="reason" id="reason_change-life" checked={inReason === `${giftedName} está passando por uma grande mudança de vida e mudar de casa é uma parte disso`} onChange={() =>setInReason(`${giftedName} está passando por uma grande mudança de vida e mudar de casa é uma parte disso`)} />
+                <label htmlFor="reason_change-life">{giftedName} está passando por uma grande mudança de vida e mudar de casa é uma parte disso</label>
             </div>
             <div>
-                <input type="radio" name="reason" id="reason_aproved" checked={inReason === "Aprovado na faculdade, mestrado ou doutorado"} onChange={() => changeInReason("Aprovado na faculdade, mestrado ou doutorado")} />
-                <label htmlFor="reason_aproved">Aprovado na faculdade, mestrado ou doutorado</label>
+                <input type="radio" name="reason" id="reason_breaking" checked={inReason === `${giftedName} está se separando`} onChange={() => setInReason(`${giftedName} está se separando`)} />
+                <label htmlFor="reason_breaking">{giftedName} está se separando</label>
             </div>
-
-            {inReason === "Aprovado na faculdade, mestrado ou doutorado" && 
-                <div>
-                    <h3>Qual dos três?</h3>
-                    <div>  
-                        <input type="radio" name="reason-begining" id="reason-complement_begining-graduation" checked={complement === "Faculdade"} onChange={() => setComplement("Faculdade")} />
-                        <label htmlFor="reason-complement_begining-graduation">Faculdade</label>
-                    </div>
-                    <div>  
-                        <input type="radio" name="reason-begining" id="reason-complement_begining-master" checked={complement === "Mestrado"} onChange={() => setComplement("Mestrado")} />
-                        <label htmlFor="reason-complement_begining-master">Mestrado</label>
-                    </div>
-                    <div>  
-                        <input type="radio" name="reason-begining" id="reason-complement_begining-phd" checked={complement === "Doutorado"} onChange={() => setComplement("Doutorado")} />
-                        <label htmlFor="reason-complement_begining-phd">Doutorado</label>
-                    </div>
-                    <br/>
-
-                </div>
-            }
             <div>
-                <input type="radio" name="reason" id="reason_ending" checked={inReason === "Finalizando faculdade, mestrado ou doutorado"} onChange={() => changeInReason("Finalizando faculdade, mestrado ou doutorado")} />
-                <label htmlFor="reason_ending">Finalizando faculdade, mestrado ou doutorado</label>
+                <input type="radio" name="reason" id="reason_back-parents" checked={inReason === `${giftedName} está voltando a morar com os pais`} onChange={() => setInReason(`${giftedName} está voltando a morar com os pais`)} />
+                <label htmlFor="reason_back-parents">{giftedName} está voltando a morar com os pais</label>
             </div>
 
-            {inReason === "Finalizando faculdade, mestrado ou doutorado" && 
-                <div>
-                    <h3>Qual dos três?</h3>
-                    <div>  
-                        <input type="radio" name="reason-ending" id="reason-complement_ending-graduation" checked={complement === "Faculdade"} onChange={() => setComplement("Faculdade")} />
-                        <label htmlFor="reason-complement_ending-graduation">Faculdade</label>
-                    </div>
-                    <div>  
-                        <input type="radio" name="reason-ending" id="reason-complement_ending-master" checked={complement === "Mestrado"} onChange={() => setComplement("Mestrado")} />
-                        <label htmlFor="reason-complement_ending-master">Mestrado</label>
-                    </div>
-                    <div>  
-                        <input type="radio" name="reason-ending" id="reason-complement_ending-phd" checked={complement === "Doutorado"} onChange={() => setComplement("Doutorado")} />
-                        <label htmlFor="reason-complement_ending-phd">Doutorado</label>
-                    </div>
-                    <br/>
-                </div>
-            }
-
             <div>
-                <input type="radio" name="reason" id="reason_newpath" checked={inReason === "Decidiu seguir um novo caminho profissional"} onChange={() => changeInReason("Decidiu seguir um novo caminho profissional")} />
-                <label htmlFor="reason_newpath">Decidiu seguir um novo caminho profissional</label>
+                <input type="radio" name="reason" id="reason_live-me" checked={inReason === `${giftedName} está vindo morar comigo`} onChange={() => setInReason(`${giftedName} está vindo morar comigo`)} />
+                <label htmlFor="reason_live-me">{giftedName} está vindo morar comigo</label>
             </div>
 
-            {inReason === "Decidiu seguir um novo caminho profissional" && 
-                <div>
-                    <input type="text" id="reason-complement_newpath" value={complement} onChange={e => setComplement(e.target.value)}/>
-                    <label htmlFor="reason-complement_newpath">Qual o novo caminho?</label>
-                </div>
-            }
 
             {warning && <p className="validation-warning">{warning}</p>}
 
             <button onClick={() => {
-                if(inReason === "Aprovado na faculdade, mestrado ou doutorado" || inReason === "Finalizando faculdade, mestrado ou doutorado" || inReason === "Decidiu seguir um novo caminho profissional") {
-                    setReason(`${inReason} -- ${complement}`);
-                    setWorkPage(0)
-                } else {
-                    setReason(inReason);
-                    setWorkPage(0)
-                }
+                setReason(inReason);
+                setMovingPage(0)
             }}>Anterior</button>
             <button onClick={() => {
-                if(inReason === "Aprovado na faculdade, mestrado ou doutorado" || inReason === "Finalizando faculdade, mestrado ou doutorado" || inReason === "Decidiu seguir um novo caminho profissional") {
-                    setReason(`${inReason} -- ${complement}`);
-                    setWorkPage(2)
+                if(!inReason) {
+                    setWarning("Por favor, escolha uma das opções")
                 } else {
                     setReason(inReason);
-                    setWorkPage(2)
+                    setMovingPage(2)
                 }
             }}>Próxima</button>
 
