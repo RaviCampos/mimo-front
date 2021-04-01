c42e6d33c76b2083b6ee4ecc9da201d74f34b796
Ravi Campos <rnscampos@gmail.com>
2021-04-01 19:52:25 -0300

    intimacyq in moving section with validation

--- src/components/pages/questions/moving/individual-questions/IntimacyQ.js
+++ src/components/pages/questions/moving/individual-questions/IntimacyQ.js
@@ -1,94 +1,87 @@
-import { useState, useEffect } from "react"
-function IntimacyQ({tools: { setWorkPage, intimacy, setIntimacy, giftedName }}) {
+import { useState, useEffect } from "react";
 
-    let protoIntimacy
+function IntimacyQ({tools: { setMovingPage, intimacy, setIntimacy, giftedName }}) {
 
-    if(intimacy) {
-        protoIntimacy = intimacy.split(" -- ")
-        if(protoIntimacy.length === 1) {
-            protoIntimacy.push("")
-        }
-    } else {
-        protoIntimacy = [ "", "" ]
-    }
-
-    const [ inIntimacy, setInIntimacy ] = useState(protoIntimacy[0])
-    const [ complement, setComplement ] = useState(protoIntimacy[1])
-
-    const [ warning, setWarning ] = useState(false)
-
-    useEffect(() => {
-        setWarning(false)
-    }, [inIntimacy, complement])
+    const [ inIntimacy, setInIntimacy ] = useState(intimacy ? intimacy : 5);
     
-    function changeInIntimacy(input) {
-        setInIntimacy(input);
-        setComplement("")
-    } 
+    useEffect(() => {
+        if(inIntimacy === 9) {
+            document.querySelector(".slider-bar").style.width = "95%";
+        } else {
+            const defaultWidthFactor = inIntimacy === 10 ? 9 : inIntimacy;
+            const defaultWidth = ( defaultWidthFactor - 1 ) * (document.querySelector(".slider-house").getBoundingClientRect().width - 3) / 8;
+            document.querySelector(".slider-bar").style.width = defaultWidth + "px"
+        }
+        
+        function handleSlideBar(event) {
+            const domBar = document.querySelector(".slider-bar");
+            const slider = document.querySelector(".slider-house");
+            const breakWidthMinus3 = slider.getBoundingClientRect().width - 3;
+            // -3 px to centralize the little vertical bar in the end of the slider, a "handle"
+            const breakBarWidth = event.pageX - slider.getBoundingClientRect().x - 3;
+            if(event.button === 0) {
+    
+                domBar.style.width = breakBarWidth + "px";
+    
+                function move(e) {
+                    if(e.buttons === 0) {
+                        window.removeEventListener("mousemove", move);
+                    } else {
+                        e.preventDefault();
+                        let breakBWidth = e.pageX - slider.getBoundingClientRect().x - 3;
+                        // check if cursor is out of the box and not increase or decrease max min values
+                        if(breakBWidth > breakWidthMinus3) {
+                            breakBWidth = breakWidthMinus3;
+                        }
+                        if(e.pageX < slider.getBoundingClientRect().x - 3) breakBWidth = 0;
 
-    function capitalize(string) {
-        return string[0].toUpperCase() + string.slice(1)
-    }
+                        domBar.style.width = breakBWidth + "px";
+                        
+                        let rating = (breakBWidth * 8 / breakWidthMinus3) + 1
+                        if(rating === 9) rating++;
+                        rating = Math.ceil(rating);
+                        setInIntimacy(rating);
+                    }
+                }
+                let rating = (breakBarWidth * 8 / breakWidthMinus3) + 1
+                if(rating === 9) rating++;
+                rating = Math.ceil(rating);
+                setInIntimacy(rating)
+
+                window.addEventListener("mousemove", move)
+            }
+        }
+
+        const sliderBreakHouse = document.querySelector(".slider-house");
+        sliderBreakHouse.addEventListener("mousedown", handleSlideBar);
+    }, [])
 
     return (
-        <div>
-            <h2>O quão próximos você e {giftedName} são?</h2>
-            <div>
-                <input type="radio" name="intimacy" id="work-intimacy_friends" checked={inIntimacy === "Somos muito amigos"} onChange={() => changeInIntimacy("Somos muito amigos")} />
-                <label htmlFor="work-intimacy_friends">Somos muito amigos</label>
-            </div>
-            <div>
-                <input type="radio" name="intimacy" id="work-intimacy_mates" checked={inIntimacy === "Eramos colegas de trabalho"} onChange={() => changeInIntimacy("Eramos colegas de trabalho")} />
-                <label htmlFor="work-intimacy_mates">Eramos colegas de trabalho</label>
-            </div>
-            <div>
-                <input type="radio" name="intimacy" id="work-intimacy_together" checked={inIntimacy === "Trabalhamos juntos"} onChange={() => changeInIntimacy("Trabalhamos juntos")} />
-                <label htmlFor="work-intimacy_together">Trabalhamos juntos</label>
-            </div>
-            <div>
-                <input type="radio" name="intimacy" id="work-intimacy_boss" checked={inIntimacy === `${capitalize(giftedName)} é meu/minha chefe`} onChange={() => changeInIntimacy(`${capitalize(giftedName)} é meu/minha chefe`)} />
-                <label htmlFor="work-intimacy_boss">{capitalize(giftedName)} é meu/minha chefe</label>
-            </div>
-            <div>
-                <input type="radio" name="intimacy" id="work-intimacy_employe" checked={inIntimacy === `${capitalize(giftedName)} trabalha para mim`} onChange={() => changeInIntimacy(`${capitalize(giftedName)} trabalha para mim`)} />
-                <label htmlFor="work-intimacy_employe">{capitalize(giftedName)} trabalha para mim</label>
-            </div>
-            <div>
-                <input type="radio" name="intimacy" id="work-intimacy_family" checked={inIntimacy === `${capitalize(giftedName)} é da família`} onChange={() => changeInIntimacy(`${capitalize(giftedName)} é da família`)} />
-                <label htmlFor="work-intimacy_family">{capitalize(giftedName)} é da família</label>
-            </div>
-            {inIntimacy === `${capitalize(giftedName)} é da família` && 
-                <div>
-                    <label htmlFor="work-intimacy_complement">Qual membro</label>
-                    <input type="text" id="work-intimacy_complement" value={complement} onChange={e => setComplement(e.target.value)}/>
+        <div className="intimacyQ">
+            <h2>Agora me conta uma coisa, só entre a gente, o quão bem você conhece {giftedName}</h2>
+            <p>Sendo <span>1</span> não conheço muito bem {giftedName}, e <span>10</span> conheço tudo de {giftedName}! Se bobiar consigo até ler a mente dessa pessoa</p>
+            <span>1</span>
+            <div className="slider-house">
+                <div className="shade"></div>
+                <div className="slider">
+                    <div className="slider-bar"></div>
+                    <div className="slider-ball"></div>
                 </div>
-            }
-
-            {warning && <p className="validation-warning">{warning}</p>}
+            </div>
+            <span>10</span>
+            <br/>
+            <span>{inIntimacy}</span>
+            
+            <br/>
 
             <button onClick={() => {
-                if(inIntimacy === `${capitalize(giftedName)} é da família`) {
-                    setIntimacy(`${inIntimacy} -- ${complement}`);
-                    setWorkPage(1)
-                } else {
-                    setIntimacy(inIntimacy);
-                    setWorkPage(1)
-                }
+                setIntimacy(inIntimacy);
+                setMovingPage(3)
             }}>Anterior</button>
             <button onClick={() => {
-                if(inIntimacy === `${capitalize(giftedName)} é da família` && !complement) {
-                    setWarning("Por favor, escreve qual o membro da família")
-                } else if(!inIntimacy) {
-                    setWarning("Por favor, selecione uma das opções")
-                } else if(inIntimacy === `${capitalize(giftedName)} é da família`) {
-                    setIntimacy(`${inIntimacy} -- ${complement}`);
-                    setWorkPage(3)
-                } else {
-                    setIntimacy(inIntimacy);
-                    setWorkPage(3)
-                }
+                setIntimacy(inIntimacy);
+                setMovingPage(5)
             }}>Próxima</button>
-
         </div>
     )
 }
