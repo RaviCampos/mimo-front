0d0130fee290f8ac8cee8d618de3c4dca2bd9f6a
Ravi Campos <rnscampos@gmail.com>
2021-04-01 12:28:15 -0300

    areaq in work section

--- src/components/pages/questions/work/individual-questions/AreaQ.js
+++ src/components/pages/questions/work/individual-questions/AreaQ.js
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react";
 
-function AreaQ() {
+function AreaQ({tools: { setWorkPage, area, setArea, giftedName }}) {
+
+    const [ inArea, setInArea ] = useState(area)
+
+    const [ warning, setWarning ] = useState(false)
+    useEffect(() => {
+        setWarning(false)
+    }, [inArea])
+
     return (
         <div>
-            
+
+            <h2>Área de atuação</h2>
+
+            {warning && <p className="validation-warning">{warning}</p>}
+
+            <button onClick={() => {
+                setArea(inArea);
+                setWorkPage(3)
+            }}>Anterior</button>
+            <button onClick={() => {
+                setArea(inArea);
+                setWorkPage(5)
+            }}>Próxima</button>
         </div>
     )
 }
