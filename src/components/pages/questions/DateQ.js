6d88df10c6809491ce015b6fcc1d2ab0e0d3f1f1
Ravi Campos <rnscampos@gmail.com>
2021-04-13 11:27:00 -0300

    dateq in common section with proper validation and facultative question

--- src/components/pages/questions/DateQ.js
+++ src/components/pages/questions/DateQ.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-function DateQ({tools: {setPage, setDate, setDeliveryBDay, date, deliveryBDay}}) {
+function DateQ({tools: {setPage, setDate, setDeliveryBDay, date, deliveryBDay, occasion}}) {
 
     const [ inDate, setInDate ] = useState(date ? date : getDatePlusFive)
     const [ inDelivery, setInDelivery ] = useState(deliveryBDay ? deliveryBDay : "")
@@ -34,12 +34,12 @@ function DateQ({tools: {setPage, setDate, setDeliveryBDay, date, deliveryBDay}})
         return false
     }
 
-    function compareAndSet(e) {
-        if(compareDates(e.target.value)) {
-            setWarning("A data precisa precisa ser pelo menos daqui a 5 dias a partir de hoje");
-        }
-        setInDate(e.target.value)
-    }
+    // function compareAndSet(e) {
+    //     if(compareDates(e.target.value)) {
+    //         setWarning("A data precisa precisa ser pelo menos daqui a 5 dias a partir de hoje");
+    //     }
+    //     setInDate(e.target.value)
+    // }
 
     useEffect(() => {
         const plusFive = getDatePlusFive();
@@ -56,21 +56,27 @@ function DateQ({tools: {setPage, setDate, setDeliveryBDay, date, deliveryBDay}})
         }
     }, [inDelivery])
 
+
+
     return (
         <div>
             <h2>Em qual dia você deseja que o mimolino entregue o presente?</h2>
             <p>a gente trabalha com um tempo de antecedência de pelo menos 5 dias</p>
-            <input type="date" name="date_date" id="gift_date" min={getDatePlusFive()} value={inDate} onChange={e => compareAndSet(e)}/>
+            <input type="date" name="date_date" id="gift_date" min={getDatePlusFive()} value={inDate} onChange={e => setInDate(e.target.value)}/>
 
-            <h2>Mais uma coisa, esse é o dia do aniversário mesmo?</h2>
-            <div>
-                <input type="radio" name="date_yesno" id="date_yes" value="Sim" checked={inDelivery === "Sim"} onChange={e => setInDelivery(e.target.value)}/>
-                <label htmlFor="date_yes">É o dia do aniversário mesmo</label>
-            </div>
-            <div>
-                <input type="radio" name="date_yesno" id="date_no" value="Não" checked={inDelivery === "Não"} onChange={e => setInDelivery(e.target.value)}/>
-                <label htmlFor="date_no">Não é exatamente o dia, mas é melhor entregar nessa data</label>
-            </div>
+            { (occasion === "aniversario" || occasion === "casamento/namoro") &&
+                <div>
+                    {occasion === "casamento/namoro" ? <h2>Mais uma coisa, esse é o dia do aniversário mesmo?</h2> : <h2>Mais uma coisa, esse é o dia do aniversário do casal mesmo?</h2> }
+                    <div>
+                        <input type="radio" name="date_yesno" id="date_yes" value="Sim" checked={inDelivery === "Sim"} onChange={e => setInDelivery(e.target.value)}/>
+                        <label htmlFor="date_yes">É o dia do aniversário mesmo</label>
+                    </div>
+                    <div>
+                        <input type="radio" name="date_yesno" id="date_no" value="Não" checked={inDelivery === "Não"} onChange={e => setInDelivery(e.target.value)}/>
+                        <label htmlFor="date_no">Não é exatamente o dia, mas é melhor entregar nessa data</label>
+                    </div>
+                </div> 
+            }
 
             {warning && <p className="validation-warning">{warning}</p>}
 
@@ -81,8 +87,8 @@ function DateQ({tools: {setPage, setDate, setDeliveryBDay, date, deliveryBDay}})
             }}>Anterior</button>
             <button onClick={() => {
                 if(compareDates(inDate)) {
-                    return
-                } else if(!inDelivery) {
+                    setWarning("A data de entrega precisa ser para daqui a pelo menos 5 dias")
+                } else if(!inDelivery && (occasion === "aniversario" || occasion === "casamento/namoro") ) {
                     setWarning("Por favor, indique se este é o dia do aniversário ou não")
                 } else {
                     setDate(inDate);
