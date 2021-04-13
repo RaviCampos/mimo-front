63b4472615c85fc20d8f1499c7685e2f8f9a9cfe
Ravi Campos <rnscampos@gmail.com>
2021-04-13 17:42:45 -0300

    taking smtpjs out

--- src/components/pages/questions/EndQ.js
+++ src/components/pages/questions/EndQ.js
@@ -1,5 +1,6 @@
 
 function EndQ({tools: {setPage, formInfo}}) {
+ 
     return (
         <div>
             <h2>Obrigado por comprar com o mimolino</h2>            
@@ -9,15 +10,7 @@ function EndQ({tools: {setPage, formInfo}}) {
                     setPage(9)
             }}>Anterior</button>
             <button onClick={() => {
-                Email.send({
-                    SecureToken : "487ca466-f07a-4a1e-af0f-3cc5857ca7aa",
-                    To : 'mimolinobsb@gmail.com',
-                    From : "mimolinobsb@gmail.com",
-                    Subject : "Nova Mimo Compra",
-                    Body : JSON.stringify(formInfo)
-                }).then(
-                    message => console.log(message)
-                );
+                
             }}>Enviar</button>
         </div>
     )
