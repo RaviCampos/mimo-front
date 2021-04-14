7e1a5e971796c43129868afc34e45b184fea9f9b
Ravi Campos <rnscampos@gmail.com>
2021-04-14 18:54:07 -0300

    basic landing page styles

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -16,19 +16,22 @@ function Landing() {
                 <div className="option-box center">
                     <label htmlFor="group-gift">O seu presente são vários presentes para os membros de um grupo de trabalho, turma da escola/faculdade, grupo de amigos</label>
                     <br/>
-                    <Link to="/group-gift"><button  id="group-gift">Presentes para um grupo</button></Link>
+                    <Link to="/group-gift"><button disabled id="group-gift">Presentes para um grupo</button></Link>
+                    <p className="remove">Essa seção estará disponível em breve</p>
                 </div>
 
                 <div className="option-box center">
                     <label htmlFor="season-gift">O seu presente é para uma data do ano como Dia das Mães, Dia dos Pais, Natal, Páscoa, Dia dos Namorados</label>
                     <br/>
-                    <Link to="/season-gift"><button id="season-gift">Presente para uma data comemorativa</button></Link>
+                    <Link to="/season-gift"><button disabled id="season-gift">Presente para uma data comemorativa</button></Link>
+                    <p className="remove">Essa seção estará disponível em breve</p>
                 </div>
                 
                 <div className="option-box center">
                     <label htmlFor="self-gift">O seu presente é para você mesmo, deixa o mimolino te dar um presente que só quem te conhece poderia te dar</label>
                     <br/>
-                    <Link to="/self-gift"><button id="self-gift">Presente para eu mesmo</button></Link>
+                    <Link to="/self-gift"><button disabled id="self-gift">Presente para eu mesmo</button></Link>
+                    <p className="remove">Essa seção estará disponível em breve</p>
                 </div>
             </div>
 
