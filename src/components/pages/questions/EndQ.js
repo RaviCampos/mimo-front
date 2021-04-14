f0b9f72bb300abb489dda7a0d1c6037e38f65a11
Ravi Campos <rnscampos@gmail.com>
2021-04-14 12:06:48 -0300

    emailjs and email at the end

--- src/components/pages/questions/EndQ.js
+++ src/components/pages/questions/EndQ.js
@@ -1,16 +1,29 @@
+import emailjs from 'emailjs-com';
 
 function EndQ({tools: {setPage, formInfo}}) {
  
+    function sendEmail() {
+        emailjs.send(
+            "service_a7ydngj", 
+            "template_8xqrqek", 
+            {message: JSON.stringify(formInfo, null, "\t")}, 
+            "user_zPbvgXcdu1HcYNxPkcMOc"
+        ).then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+        }, function(error) {
+            console.log('FAILED...', error);
+        });
+    }
     return (
         <div>
             <h2>Obrigado por comprar com o mimolino</h2>            
             <h3>Talvez revise seus dados</h3>
             <p>{JSON.stringify(formInfo)}</p>
             <button onClick={() => {
-                    setPage(9)
+                setPage(9)
             }}>Anterior</button>
             <button onClick={() => {
-                
+               sendEmail();
             }}>Enviar</button>
         </div>
     )
