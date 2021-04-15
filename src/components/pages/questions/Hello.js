642a81a407b0643e465ec7990eb1fa0b8aff1e03
Ravi Campos <rnscampos@gmail.com>
2021-04-14 21:01:00 -0300

    hello with page  margin

--- src/components/pages/questions/Hello.js
+++ src/components/pages/questions/Hello.js
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom";
 
 function Hello({ tools: { setPage }}) {
     return (
-        <div>
+        <div className="all_margin hello">
             <h1>Mimo questions</h1>
             <h2>Ouvi dizer que você está procurando por um presente, certo?</h2>
-            <p>O Mimolino pode te ajudar a encontrar O melhor presente possível! Como?</p>
-            <p>É só responder a este formulário com sinceridade. A partir das suas respostas vamos te enviar 3 opções incríveis de presentes. Aí só você escolher entre elas e aproveitar a sensação que só um bom presente pode provocar.</p>
-            <p>no final das perguntas você vai poder optar por falar com a gente por whatsapp, dm no Instagram, ou email daqui para a frente</p>
+            <p className="box-paragraph">O Mimolino pode te ajudar a encontrar O melhor presente possível! Como?</p>
+            <p className="box-paragraph">É só responder a este formulário com sinceridade. A partir das suas respostas vamos te enviar 3 opções incríveis de presentes. Aí só você escolher entre elas e aproveitar a sensação que só um bom presente pode provocar.</p>
+            <p className="box-paragraph">No final das perguntas você vai poder optar por falar com a gente por whatsapp, dm no Instagram, ou email daqui para a frente</p>
 
-            <Link to="/"><button>Voltar para a página inicial</button></Link>
             <button onClick={() => setPage(1)}>Começar a responder!</button>
+            <Link to="/"><button>Voltar para a página inicial</button></Link>
 
         </div>
     )
