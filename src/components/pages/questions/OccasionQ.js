be1690aeabaa6ce83aab1cf71d72c6eae612639c
Ravi Campos <rnscampos@gmail.com>
2021-04-15 20:55:30 -0300

    custom radio button

--- src/components/pages/questions/OccasionQ.js
+++ src/components/pages/questions/OccasionQ.js
@@ -16,36 +16,40 @@ function OccasionQ({ tools: { setPage, setSection, setOccasion, occasion }}) {
             <p className="subtitle">(não que deva existir ocasião especial para dar presente! kkkk)</p>
 
             <div>
-                <div className="radio-option">
+                <label className="radio-option">Sim, é aniversário desta pessoa!
                     <input type="radio" name="occasion" id="occ_bday" value="aniversario" checked={radioValue === "aniversario"} onChange={e=> setRadioValue(e.target.value)}/>
-                    <div className="inline">
-                        <label htmlFor="occ_bday">Sim, é aniversário desta pessoa!</label>
-                    </div>
-                </div>
-                <div className="radio-option">
+                    <span class="checkmark"></span>
+                </label>
+                <label className="radio-option">
+                    Sim, é aniversário de namoro/casamento
                     <input type="radio" name="occasion" id="occ_wedding" value="casamento/namoro" checked={radioValue === "casamento/namoro"} onChange={e => setRadioValue(e.target.value) }/>
-                    <label htmlFor="occ_wedding">Sim, é aniversário de namoro/casamento</label>
-                </div>
-                <div className="radio-option">
+                    <span class="checkmark"></span>
+                </label>
+                <label className="radio-option">
+                    Sim, acabamos de descobrir que essa pessoa vai ter um filho
                     <input type="radio" name="occasion" id="occ_offspring" value="bebe" checked={radioValue === "bebe"} onChange={e=> setRadioValue(e.target.value)}/>
-                    <label htmlFor="occ_offspring">Sim, acabamos de descobrir que essa pessoa vai ter um filho</label>
-                </div>
-                <div className="radio-option">
+                    <span class="checkmark"></span>
+                </label>
+                <label className="radio-option">
+                    Sim, essa pessoa está atingindo uma conquista profissional/acadêmica
                     <input type="radio" name="occasion" id="occ_promotion" value="trabalho" checked={radioValue === "trabalho"} onChange={e=> setRadioValue(e.target.value)}/>
-                    <label htmlFor="occ_promotion">Sim, essa pessoa está atingindo uma conquista profissional/acadêmica</label>
-                </div>
-                <div className="radio-option">
+                    <span class="checkmark"></span>
+                </label>
+                <label className="radio-option">
+                    Sim, essa pessoa está se mudando para uma casa nova
                     <input type="radio" name="occasion" id="occ_house" value="mudança" checked={radioValue === "mudança"} onChange={e=> setRadioValue(e.target.value)}/>
-                    <label htmlFor="occ_house">Sim, essa pessoa está se mudando para uma casa nova</label>
-                </div>
-                <div className="radio-option">
+                    <span class="checkmark"></span>
+                </label>
+                <label className="radio-option">
+                    Sim, vai fazer ou está planejando uma viagem
                     <input type="radio" name="occasion" id="occ_trip" value="viagem" checked={radioValue === "viagem"} onChange={e=> setRadioValue(e.target.value)}/>
-                    <label htmlFor="occ_trip">Sim, vai fazer ou está planejando uma viagem</label>
-                </div>
-                <div className="radio-option">
+                    <span class="checkmark"></span>
+                </label>
+                <label className="radio-option">
+                    Claro que não, não preciso de ocasiões especiais para dar presentes!
                     <input type="radio" name="occasion" id="occ_nop" value="nenhuma" checked={radioValue === "nenhuma"} onChange={e=> setRadioValue(e.target.value)}/>
-                    <label htmlFor="occ_nop">Claro que não, não preciso de ocasiões especiais para dar presentes!</label>
-                </div>
+                    <span class="checkmark"></span>
+                </label>
             </div>
 
             {warning && <p className="validation-warning">{warning}</p>}
