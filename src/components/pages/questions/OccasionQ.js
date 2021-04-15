c56e2881775707e599886c7759181e86d47d8eb9
Ravi Campos <rnscampos@gmail.com>
2021-04-15 10:34:12 -0300

    furthering occasion efforts

--- src/components/pages/questions/OccasionQ.js
+++ src/components/pages/questions/OccasionQ.js
@@ -11,36 +11,38 @@ function OccasionQ({ tools: { setPage, setSection, setOccasion, occasion }}) {
     }, [radioValue])
 
     return (
-        <div>
-            <h2>Esse presente é para alguma ocasião especial? Se sim, qual?</h2>
-            <p>(não que deva existir ocasião especial para dar presente! kkkk)</p>
+        <div className="all-margin">
+            <h2 className="title">Esse presente é para alguma ocasião especial? Se sim, qual?</h2>
+            <p className="subtitle">(não que deva existir ocasião especial para dar presente! kkkk)</p>
 
             <div>
-                <div>
+                <div className="radio-option">
                     <input type="radio" name="occasion" id="occ_bday" value="aniversario" checked={radioValue === "aniversario"} onChange={e=> setRadioValue(e.target.value)}/>
-                    <label htmlFor="occ_bday">Sim, é aniversário desta pessoa!</label>
+                    <div className="inline">
+                        <label htmlFor="occ_bday">Sim, é aniversário desta pessoa!</label>
+                    </div>
                 </div>
-                <div>
+                <div className="radio-option">
                     <input type="radio" name="occasion" id="occ_wedding" value="casamento/namoro" checked={radioValue === "casamento/namoro"} onChange={e => setRadioValue(e.target.value) }/>
                     <label htmlFor="occ_wedding">Sim, é aniversário de namoro/casamento</label>
                 </div>
-                <div>
+                <div className="radio-option">
                     <input type="radio" name="occasion" id="occ_offspring" value="bebe" checked={radioValue === "bebe"} onChange={e=> setRadioValue(e.target.value)}/>
                     <label htmlFor="occ_offspring">Sim, acabamos de descobrir que essa pessoa vai ter um filho</label>
                 </div>
-                <div>
+                <div className="radio-option">
                     <input type="radio" name="occasion" id="occ_promotion" value="trabalho" checked={radioValue === "trabalho"} onChange={e=> setRadioValue(e.target.value)}/>
                     <label htmlFor="occ_promotion">Sim, essa pessoa está atingindo uma conquista profissional/acadêmica</label>
                 </div>
-                <div>
+                <div className="radio-option">
                     <input type="radio" name="occasion" id="occ_house" value="mudança" checked={radioValue === "mudança"} onChange={e=> setRadioValue(e.target.value)}/>
                     <label htmlFor="occ_house">Sim, essa pessoa está se mudando para uma casa nova</label>
                 </div>
-                <div>
+                <div className="radio-option">
                     <input type="radio" name="occasion" id="occ_trip" value="viagem" checked={radioValue === "viagem"} onChange={e=> setRadioValue(e.target.value)}/>
                     <label htmlFor="occ_trip">Sim, vai fazer ou está planejando uma viagem</label>
                 </div>
-                <div>
+                <div className="radio-option">
                     <input type="radio" name="occasion" id="occ_nop" value="nenhuma" checked={radioValue === "nenhuma"} onChange={e=> setRadioValue(e.target.value)}/>
                     <label htmlFor="occ_nop">Claro que não, não preciso de ocasiões especiais para dar presentes!</label>
                 </div>
@@ -48,19 +50,21 @@ function OccasionQ({ tools: { setPage, setSection, setOccasion, occasion }}) {
 
             {warning && <p className="validation-warning">{warning}</p>}
 
-            <button onClick={() => {
-                setOccasion(radioValue);
-                setPage(1)
-            }}>Anterior</button>
-            <button onClick={() => {
-                if(radioValue) {
-                    setSection("occasion")
+            <div className="prev-for">
+                <button onClick={() => {
                     setOccasion(radioValue);
-                    setPage(3)
-                } else {
-                    setWarning("Por favor, selecione uma das opções para prosseguir")
-                }
-            }}>Próxima</button>
+                    setPage(1)
+                }}>Anterior</button>
+                <button onClick={() => {
+                    if(radioValue) {
+                        setSection("occasion")
+                        setOccasion(radioValue);
+                        setPage(3)
+                    } else {
+                        setWarning("Por favor, selecione uma das opções para prosseguir")
+                    }
+                }}>Próxima</button>
+            </div>
         </div>
     )
 }
