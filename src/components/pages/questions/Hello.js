d8d1d5264b4176aef9b76462f041298fe4887f19
Ravi Campos <rnscampos@gmail.com>
2021-04-14 22:22:07 -0300

    nameq basic styles

--- src/components/pages/questions/Hello.js
+++ src/components/pages/questions/Hello.js
@@ -6,7 +6,7 @@ function Hello({ tools: { setPage }}) {
             <h1>Mimo questions</h1>
             <h2>Ouvi dizer que você está procurando por um presente, certo?</h2>
             <p className="box-paragraph">O Mimolino pode te ajudar a encontrar O melhor presente possível! Como?</p>
-            <p className="box-paragraph">É só responder a este formulário com sinceridade. A partir das suas respostas vamos te enviar 3 opções incríveis de presentes. Aí só você escolher entre elas e aproveitar a sensação que só um bom presente pode provocar.</p>
+            <p className="box-paragraph">É só responder às próximas perguntas com sinceridade. A partir das suas respostas vamos te enviar 3 opções incríveis de presentes. Aí só você escolher entre elas e aproveitar a sensação que só um bom presente pode provocar.</p>
             <p className="box-paragraph">No final das perguntas você vai poder optar por falar com a gente por whatsapp, dm no Instagram, ou email daqui para a frente</p>
 
             <button onClick={() => setPage(1)}>Começar a responder!</button>
--- src/components/pages/questions/NameQ.js
+++ src/components/pages/questions/NameQ.js
@@ -13,30 +13,32 @@ function GifterNameQ({ tools: { setPage, setGifterName, gifterName }}) {
     }, [inGifter])
 
     return (
-        <div>
-            <h1>{inGifter}</h1>
-            
-            <h2>Qual é o seu nome?</h2>
+        <div className="all-margin">
+
+            <h2 className="title">Qual é o seu nome?</h2>
             <input type="text" value={inGifter} onChange={e => setInGifter(e.target.value)}/>
             {showWarning && <p className="validation-warning">preencha o nome para seguir em frente</p>}
             
             <br/>
-            <button onClick={() => {
-                // if(inGifter) {
-                    setGifterName(inGifter)
-                    setPage(0)
-                // } else {
-                //     setShowWarning(true);
-                // }
-            }}>Anterior</button>
-            <button onClick={() => {
-                if(inGifter) {
-                    setGifterName(inGifter)
-                    setPage(2)    
-                } else {
-                    setShowWarning(true)
-                }
-            }}>Próxima</button>
+
+            <div className="prev-for">
+                <button onClick={() => {
+                    // if(inGifter) {
+                        setGifterName(inGifter)
+                        setPage(0)
+                    // } else {
+                    //     setShowWarning(true);
+                    // }
+                }}>Anterior</button>
+                <button onClick={() => {
+                    if(inGifter) {
+                        setGifterName(inGifter)
+                        setPage(2)    
+                    } else {
+                        setShowWarning(true)
+                    }
+                }}>Próxima</button>
+            </div>
         </div>
     )
 }
