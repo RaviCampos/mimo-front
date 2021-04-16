11fd58ddc7a6e4cc70638b23c157d926293c9506
Ravi Campos <rnscampos@gmail.com>
2021-04-16 16:03:39 -0300

    giftednameq styles

--- src/components/pages/questions/bday/individual-questions/GiftedNameQ.js
+++ src/components/pages/questions/bday/individual-questions/GiftedNameQ.js
@@ -12,32 +12,36 @@ function GiftedNameQ({tools: {setPage, setBDayPage, setGiftedName, giftedName, s
     }, [inGifted])
 
     return (
-        <div>
-            <h1>{inGifted}</h1>
-            
-            <h2>Qual é o nome da pessoa que vai ganhar o presente?</h2>
-            <input type="text" value={inGifted} onChange={e => setInGifted(e.target.value)}/>
-            {showWarning && <p className="validation-warning">preencha o nome para seguir em frente</p>}
+        <div className="all-margin">
+            <div className="all-center">
+                <div>
+                    <h2 className="title">Qual é o nome da pessoa que vai ganhar o presente?</h2>
+                    <input className="bit-down" type="text" value={inGifted} onChange={e => setInGifted(e.target.value)}/>
+                    {showWarning && <p className="validation-warning">preencha o nome para seguir em frente</p>}
 
-            <br/>
-            <button onClick={() => {
-                const bday = {
-                    ...futureBDay,
-                    giftedName: inGifted
-                }
-                setBDay(bday)
-                setGoToOccasionLastQ(false)
-                setSection("common")
-                setPage(2);
-            }}>Anterior</button>
-            <button onClick={() => {
-                if(inGifted) {
-                    setGiftedName(inGifted)
-                    setBDayPage(1)
-                } else {
-                    setShowWarning(true);
-                }
-            }}>Próxima</button>
+                    <br/>
+                    <div className="prev-for go-down">
+                        <button onClick={() => {
+                            const bday = {
+                                ...futureBDay,
+                                giftedName: inGifted
+                            }
+                            setBDay(bday)
+                            setGoToOccasionLastQ(false)
+                            setSection("common")
+                            setPage(2);
+                        }}>Anterior</button>
+                        <button onClick={() => {
+                            if(inGifted) {
+                                setGiftedName(inGifted)
+                                setBDayPage(1)
+                            } else {
+                                setShowWarning(true);
+                            }
+                        }}>Próxima</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
