66f6e08112ab0c2f20b54a5f588bfe07ac79ccda
Ravi Campos <rnscampos@gmail.com>
2021-04-20 18:30:53 -0300

    foodrestrictionq pre styles

--- src/components/pages/questions/FoodRestrictionQ.js
+++ src/components/pages/questions/FoodRestrictionQ.js
@@ -36,56 +36,66 @@ function FoodRestrictionQ({tools: {setPage, setFoodRestriction, foodRestriction,
     }, [complement])
 
     return (
-        <div>
-            <h2>Falar algo como: A gente as vezes gosta de dar gostosuras comestíveis nos nossos presentes, mas a gente sabe que as comidinhas não funcionam para todas as ocasiões</h2>
-            <h2>Só por via das dúvidas você sabe se {have} alguma alergia ou restrição alimentar?</h2>
-            <div>
+        <div className="all-margin">
+            <div className="all-center">
+                <h2 className="title">Só por via das dúvidas você sabe se {have} alguma alergia ou restrição alimentar?</h2>
                 <div>
-                    <input type="radio" name="food" id="food_nop" value="não que eu saiba" checked={inRestriction === "não que eu saiba"} onChange={e=>{ setInRestriction(e.target.value); setComplement("")}}/>
-                    <label htmlFor="food_nop">Não que eu saiba</label>
-                </div>
-                <div>
-                    <input type="radio" name="food" id="food_diet" value="faz dieta e por isso não come algumas coisas" checked={inRestriction === "faz dieta e por isso não come algumas coisas"} onChange={e=> { setInRestriction(e.target.value); setComplement("")}}/>
-                    <label htmlFor="food_diet">{ondiet} dieta e por isso não come algumas coisas</label>
-                </div>
-                <div>
-                    <input type="radio" name="food" id="food_alergy" value="tem alergia ou não come de jeito nenhum" checked={inRestriction === "tem alergia ou não come de jeito nenhum"} onChange={e=> { setInRestriction(e.target.value); setComplement("")}}/>
-                    <label htmlFor="food_alergy">Sim, {have} alergia ou não come de jeito nenhum</label>
-                    {inRestriction === "tem alergia ou não come de jeito nenhum" && <input type="text" value={complement} onChange={e => setComplement(e.target.value)}/>}
-                </div>
-                <div>
-                    <input type="radio" name="food" id="food_dontwant" value="Acho que comida de presente não combina muito com a ocasião e/ou a pessoa" checked={inRestriction === "Acho que comida de presente não combina muito com a ocasião e/ou a pessoa"} onChange={e=> { setInRestriction(e.target.value); setComplement("")}}/>
-                    <label htmlFor="food_dontwant">Não acho que vem ao caso. Acho que comida de presente não combina muito com a ocasião e/ou a pessoa</label>
+                    <label className="radio-option">
+                        Não que eu saiba
+                        <input type="radio" name="food" id="food_nop" value="não que eu saiba" checked={inRestriction === "não que eu saiba"} onChange={e=>{ setInRestriction(e.target.value); setComplement("")}}/>
+                        <span className="checkmark"></span>
+                    </label>
+                    <label className="radio-option">
+                        {ondiet} dieta e por isso não come algumas coisas
+                        <input type="radio" name="food" id="food_diet" value="faz dieta e por isso não come algumas coisas" checked={inRestriction === "faz dieta e por isso não come algumas coisas"} onChange={e=> { setInRestriction(e.target.value); setComplement("")}}/>
+                        <span className="checkmark"></span>
+                    </label>
+                    <label className="radio-option">
+                        Sim, {have} alergia ou não come algumas comidas de jeito nenhum
+                        <input type="radio" name="food" id="food_alergy" value="tem alergia ou não come de jeito nenhum" checked={inRestriction === "tem alergia ou não come de jeito nenhum"} onChange={e=> { setInRestriction(e.target.value); setComplement("")}}/>
+                        <span className="checkmark"></span>
+                    </label>
+                    <div className="space-up-down">
+                        {inRestriction === "tem alergia ou não come de jeito nenhum" && <input type="text" value={complement} onChange={e => setComplement(e.target.value)} placeholder="Quais?" autoComplete="off"/>}
+                    </div>
+                    <label className="radio-option">
+                        Não acho que vem ao caso. Acho que comida de presente não combina muito com a ocasião e/ou a pessoa
+                        <input type="radio" name="food" id="food_dontwant" value="Acho que comida de presente não combina muito com a ocasião e/ou a pessoa" checked={inRestriction === "Acho que comida de presente não combina muito com a ocasião e/ou a pessoa"} onChange={e=> { setInRestriction(e.target.value); setComplement("")}}/>
+                        <span className="checkmark"></span>
+                    </label>
+
+                    {warning && <p className="validation-warning">{warning}</p>}
+
                 </div>
 
-                {warning && <p className="validation-warning">{warning}</p>}
+                <div className="prev-for go-bit-down">
 
+                    <button onClick={() => {
+                        if(complement) {
+                            const answer = `${inRestriction}& ${complement}`
+                            setFoodRestriction(answer);
+                        } else {
+                            setFoodRestriction(inRestriction);
+                        }
+                        setPage(4);
+                    }}>Anterior</button>
+                    <button onClick={() => {
+                        if(!inRestriction) {
+                            setWarning("Por favor, selecione alguma das opções")
+                        } else if(inRestriction === "tem alergia ou não come de jeito nenhum" && !complement) {
+                            setWarning(`Por favor, preencha que alimento(s) ${giftedName} não pode comer`)
+                        } else {
+                            if(complement) {
+                                const answer = `${inRestriction} & ${complement}`
+                                setFoodRestriction(answer);
+                            } else {
+                                setFoodRestriction(inRestriction);
+                            }
+                            setPage(6);
+                        }
+                    }}>Próxima</button>
+                </div>
             </div>
-
-            <button onClick={() => {
-                if(complement) {
-                    const answer = `${inRestriction}& ${complement}`
-                    setFoodRestriction(answer);
-                } else {
-                    setFoodRestriction(inRestriction);
-                }
-                setPage(4);
-            }}>Anterior</button>
-            <button onClick={() => {
-                if(!inRestriction) {
-                    setWarning("Por favor, selecione alguma das opções")
-                } else if(inRestriction === "tem alergia ou não come de jeito nenhum" && !complement) {
-                    setWarning(`Por favor, preencha que alimento(s) ${giftedName} não pode comer`)
-                } else {
-                    if(complement) {
-                        const answer = `${inRestriction} & ${complement}`
-                        setFoodRestriction(answer);
-                    } else {
-                        setFoodRestriction(inRestriction);
-                    }
-                    setPage(6);
-                }
-            }}>Próxima</button>
         </div>
     )
 }
