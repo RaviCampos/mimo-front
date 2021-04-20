6baabab307cdeab9597342f8284c143ddbf34078
Ravi Campos <rnscampos@gmail.com>
2021-04-20 17:50:33 -0300

    helloq buttons adjusted

--- src/components/pages/questions/Hello.js
+++ src/components/pages/questions/Hello.js
@@ -9,8 +9,10 @@ function Hello({ tools: { setPage }}) {
             <p className="box-paragraph">É só responder às próximas perguntas com sinceridade. A partir das suas respostas vamos te enviar 3 opções incríveis de presentes. Aí só você escolher entre elas e aproveitar a sensação que só um bom presente pode provocar.</p>
             <p className="box-paragraph">No final das perguntas você vai poder optar por falar com a gente por whatsapp, dm no Instagram, ou email daqui para a frente</p>
 
-            <button onClick={() => setPage(1)}>Começar a responder!</button>
-            <Link to="/"><button>Voltar para a página inicial</button></Link>
+            <div className="hello-buttons">
+                <button onClick={() => setPage(1)}>Começar a responder!</button>
+                <Link to="/"><button>Voltar para a página inicial</button></Link>
+            </div>
 
         </div>
     )
