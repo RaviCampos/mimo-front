2a3bc9381a12aa41785183b1ff6d80afa9ea9bed
Ravi Campos <rnscampos@gmail.com>
2021-04-21 16:59:21 -0300

    addresq pre styles

--- src/components/pages/questions/AdressQ.js
+++ src/components/pages/questions/AdressQ.js
@@ -33,25 +33,27 @@ function AdressQ({tools: { setPage, setAdress, adress, giftedName }}) {
         name = name[0]
     }
 
-    const input = <div>
+    const input = <div className="go-bit-down">
         <div>
-            <label htmlFor="adress_cep">CEP</label>
-            <input type="number" id="adress_cep" value={cep} onChange={e => setCep(e.target.value)}/>
+            <label htmlFor="adress_cep" className="subtitle d-block no-space-down">CEP</label>
+            <input type="number" id="address_cep" value={cep} onChange={e => setCep(e.target.value)} autoComplete="off" min="1" max="100000"/>
         </div>
-        <div>
-            <label htmlFor="adress_adress">Endereço</label>
-            <input type="text" id="adress_adress" value={inAdress} onChange={e => setInAdress(e.target.value)}/>
+        <div className="small-space-top">
+            <label className="subtitle" htmlFor="adress_adress">Endereço</label>
+            <textarea id="adress_adress" value={inAdress} onChange={e => setInAdress(e.target.value)} autoComplete="off"/>
         </div>
 
-        <h3>Esse presente será etregue para você ou diretamente para {name}</h3>
-        <div>
-            <label htmlFor="to_gifter">Para mim, quero eu mesmo entregar para {name} depois</label>
+        <h3 className="subtitle bit-down small-space-down">Esse presente será etregue para você ou diretamente para {name}?</h3>
+        <label className="radio-option">
+            Para mim, quero eu mesmo entregar para {name} depois
             <input type="radio" id="to_gifter" checked={to === "presenteador"} onChange={() => setTo("presenteador")}/>
-        </div>
-        <div>
-            <label htmlFor="to_gifted">Diretamente para {name}</label>
+            <span className="checkmark"></span>
+        </label>
+        <label className="radio-option">
+            Diretamente para {name}
             <input type="radio" id="to_gifted" checked={to === "presenteado"} onChange={() => setTo("presenteado")}/>
-        </div>
+            <span className="checkmark"></span>
+        </label>
     </div>
     
     function changeWithdraw(inAddressValue) {
@@ -61,52 +63,59 @@ function AdressQ({tools: { setPage, setAdress, adress, giftedName }}) {
     }
 
     return (
-        <div>
-            <h2>Você prefere retirar o presente conosco ou que ele seja entregue?</h2>
-            <p className="remove">mudar o tipo de input de cep para number sem setas, cep precisa ser requerido imperativamente ao tentar mudar de página</p>
-            <div>
-                <label htmlFor="withdraw_no">Prefiro que o presente seja entregue</label>
-                <input type="radio" name="withdraw" id="withdraw_no" checked={inAdress !== "retirada" && inAdress !== "default"} onChange={() => changeWithdraw("")}/>
-            </div>
-            <div>
-                <label htmlFor="withdraw_yes">Prefiro buscar o presente</label>
-                <input type="radio" name="withdraw" id="withdraw_yes" checked={inAdress === "retirada"} onChange={() => changeWithdraw("retirada")}/>
-            </div>
-            {inAdress === "retirada" && <p>assim que o pedido for finalizado nós entraremos em contato e informaremos o local de retirada na asa norte</p>}
-            {inAdress !== "retirada" && inAdress !== "default" && input}
+        <div className="all-margin">
+            <div className="all-center">
+                <div>
+                    <h2>Você prefere retirar o presente conosco ou que ele seja entregue?</h2>
+                    <p className="remove">mudar o tipo de input de cep para number sem setas, cep precisa ser requerido imperativamente ao tentar mudar de página</p>
+                    <label className="radio-option">
+                        Prefiro que o presente seja entregue
+                        <input type="radio" name="withdraw" id="withdraw_no" checked={inAdress !== "retirada" && inAdress !== "default"} onChange={() => changeWithdraw("")}/>
+                        <span className="checkmark"></span>
+                    </label>
+                    <label className="radio-option">
+                        Prefiro buscar o presente
+                        <input type="radio" name="withdraw" id="withdraw_yes" checked={inAdress === "retirada"} onChange={() => changeWithdraw("retirada")}/>
+                        <span className="checkmark"></span>
+                    </label>
+                    {inAdress === "retirada" && <p>assim que o pedido for finalizado nós entraremos em contato e informaremos o local de retirada na asa norte</p>}
+                    {inAdress !== "retirada" && inAdress !== "default" && input}
 
-            {warning && <p className="validation-warning">{warning}</p>}
-            
-            <button onClick={() => {
-                if(inAdress === "retirada") {
-                    setAdress(inAdress);
-                } else {
-                    setAdress(`endereço: ${inAdress} && cep: ${cep} && para: ${to}`)
-                }
-                setPage(7)
-            }}>Anterior</button>
-            <button onClick={() => {
-                if(inAdress === "default") {
-                    setWarning("Por favor, escolha se o seu presente será retirado ou entregue")
-                } else {
-                    if(inAdress === "retirada") {
-                        setAdress(inAdress);
-                        setPage(9)
-                    } else {
-                        if(!cep) {
-                            setWarning("Por favor, preencha o seu CEP")
-                        } else if(!inAdress) {
-                            setWarning("Por favor, preencha seu endereço")
-                        } else if(!to) {
-                            setWarning(`Por favor, selecione se o presente será entregue para você ou para ${name}`)
-                        } else {
-                            setAdress(`endereço: ${inAdress} && cep: ${cep} && para: ${to}`)
-                            setPage(9)
-                        }
-                    }
-                }
-            }}>Próxima</button>
-
+                    {warning && <p className="validation-warning">{warning}</p>}
+                    
+                    <div className="prev-for">
+                        <button onClick={() => {
+                            if(inAdress === "retirada") {
+                                setAdress(inAdress);
+                            } else {
+                                setAdress(`endereço: ${inAdress} && cep: ${cep} && para: ${to}`)
+                            }
+                            setPage(7)
+                        }}>Anterior</button>
+                        <button onClick={() => {
+                            if(inAdress === "default") {
+                                setWarning("Por favor, escolha se o seu presente será retirado ou entregue")
+                            } else {
+                                if(inAdress === "retirada") {
+                                    setAdress(inAdress);
+                                    setPage(9)
+                                } else {
+                                    if(!cep) {
+                                        setWarning("Por favor, preencha o seu CEP")
+                                    } else if(!inAdress) {
+                                        setWarning("Por favor, preencha seu endereço")
+                                    } else if(!to) {
+                                        setWarning(`Por favor, selecione se o presente será entregue para você ou para ${name}`)
+                                    } else {
+                                        setAdress(`endereço: ${inAdress} && cep: ${cep} && para: ${to}`)
+                                        setPage(9)
+                                    }
+                                }
+                            }
+                        }}>Próxima</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
