a12dcf508f036c3045de36b7ddae66a2967076b6
Ravi Campos <rnscampos@gmail.com>
2021-04-21 18:40:27 -0300

    endq centralized, not finish

--- src/components/pages/questions/EndQ.js
+++ src/components/pages/questions/EndQ.js
@@ -15,16 +15,23 @@ function EndQ({tools: {setPage, formInfo}}) {
         });
     }
     return (
-        <div>
-            <h2>Obrigado por comprar com o mimolino</h2>            
-            <h3>Talvez revise seus dados</h3>
-            <p>{JSON.stringify(formInfo)}</p>
-            <button onClick={() => {
-                setPage(9)
-            }}>Anterior</button>
-            <button onClick={() => {
-               sendEmail();
-            }}>Enviar</button>
+        <div className="all-margin">
+            <div className="all-center">
+                <div>
+                    <h2>Obrigado por comprar com o mimolino</h2>            
+                    <h3>Talvez revise seus dados</h3>
+                    <p>{JSON.stringify(formInfo)}</p>
+
+                    <div className="prev-for">
+                        <button onClick={() => {
+                            setPage(9)
+                        }}>Anterior</button>
+                        <button onClick={() => {
+                        sendEmail();
+                        }}>Enviar</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
