86241bcc21dc0aca2e32eb81a0ad8c3d4b3537a9
Ravi Campos <rnscampos@gmail.com>
2021-04-21 18:50:03 -0300

    giftertincoupleq pre styles

--- src/components/pages/questions/wedding/individual-questions/GifterInCoupleQ.js
+++ src/components/pages/questions/wedding/individual-questions/GifterInCoupleQ.js
@@ -10,44 +10,52 @@ function GifterInCoupleQ({ tools: { setSection, futureWedding, setWedding, setPa
     }, [inCouple])
 
     return (
-        <div>
-            <h2>Me tira uma dúvida? Vocês são um casal e é aniversário de vocês ou você está presenteando um casal amigo por um marco no relacionamento deles?</h2>
+        <div className="all-margin">
+            <div className="all-center">
+                <div>
+                    <h2 className="small-title">Me tira uma dúvida? Vocês são um casal e é aniversário de vocês ou você está presenteando um casal amigo por um marco no relacionamento deles?</h2>
 
-            <div>
-                <input type="radio" name="couple" id="couple_in" checked={inCouple === true} onChange={e => setInCouple(true)}/>
-                <label htmlFor="couple_in">Eu faço parte do casal, vamos comemorar nossa união</label>
+                    <label className="radio-option">
+                        Eu faço parte do casal, vamos comemorar nossa união
+                        <input type="radio" name="couple" id="couple_in" checked={inCouple === true} onChange={e => setInCouple(true)}/>
+                        <span className="checkmark"></span>
+                    </label>
+                    <label className="radio-option long-option">
+                        Conheço esse casal que está comemorando aniversário em seu relacionamento e não posso deixar de presenteá-los
+                        <input type="radio" name="couple" id="couple_out" checked={inCouple === false} onChange={e => setInCouple(false)}/>
+                        <span className="checkmark"></span>
+                    </label>
+
+                    {warning && <p className="validation-warning">{warning}</p>}
+
+                    <div className="prev-for">
+                        <button onClick={() => {
+                            const isInCouple =  inCouple ? "Sim" : "Não"
+                            const wed = {
+                                ...futureWedding,
+                                gifterInCouple: isInCouple
+                            }
+                            if(inCouple) {
+                                delete wed.reasonToGift
+                            } else {
+                                delete wed.commonHobbies
+                            }
+                            setWedding(wed);
+                            setGoToOccasionLastQ(false)
+                            setSection("common")
+                            setPage(2);
+                        }}>Anterior</button>
+                        <button onClick={() => {
+                            if(inCouple !== "") {
+                                setGifterInCouple(inCouple);
+                                setWeddingPage(1)
+                            } else {
+                                setWarning("Por favor, selecione uma das opções")
+                            }
+                        }}>Próxima</button>
+                    </div>
+                </div>
             </div>
-            <div>
-                <input type="radio" name="couple" id="couple_out" checked={inCouple === false} onChange={e => setInCouple(false)}/>
-                <label htmlFor="couple_out">Conheço esse casal que está comemorando aniversário em seu relacionamento e não posso deixar de presenteá-los</label>
-            </div>
-
-            {warning && <p className="validation-warning">{warning}</p>}
-
-            <button onClick={() => {
-                const isInCouple =  inCouple ? "Sim" : "Não"
-                const wed = {
-                    ...futureWedding,
-                    gifterInCouple: isInCouple
-                }
-                if(inCouple) {
-                    delete wed.reasonToGift
-                } else {
-                    delete wed.commonHobbies
-                }
-                setWedding(wed);
-                setGoToOccasionLastQ(false)
-                setSection("common")
-                setPage(2);
-            }}>Anterior</button>
-            <button onClick={() => {
-                if(inCouple !== "") {
-                    setGifterInCouple(inCouple);
-                    setWeddingPage(1)
-                } else {
-                    setWarning("Por favor, selecione uma das opções")
-                }
-            }}>Próxima</button>
 
         </div>
     )
