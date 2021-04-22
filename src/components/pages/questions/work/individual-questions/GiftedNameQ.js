6bc8b55cf8219e4319c5eba34c2eec33ecaa1af8
Ravi Campos <rnscampos@gmail.com>
2021-04-22 14:52:03 -0300

    work giftedname pre styles

--- src/components/pages/questions/work/individual-questions/GiftedNameQ.js
+++ src/components/pages/questions/work/individual-questions/GiftedNameQ.js
@@ -10,38 +10,44 @@ function GiftedNameQ({tools: { setSection, futureWork, setWork, setPage, setGoTo
     }, [inName])
     
     return (
-        <div>
-            <h2>Comemorar conquistas é sempre muito importante! Qual é o nome da pessoa que vai receber o presente?</h2>
-            <input type="text" value={inName} onChange={e => setInName(e.target.value)}/>
+        <div className="all-margin">
+            <div className="all-center">
+                <div>
+                    <h2 className="title">Comemorar conquistas é sempre muito importante! Qual é o nome da pessoa que vai receber o presente?</h2>
+                    <input type="text" value={inName} onChange={e => setInName(e.target.value)} autoComplete="off"/>
 
-            <br/>
+                    <br/>
 
-            {warning && <p className="validation-warning">{warning}</p>}
-            
-            <button onClick={() => {
-                const work = {
-                    ...futureWork,
-                    giftedName: inName
-                }
-                // if(inCouple) {
-                //     delete wed.reasonToGift
-                // } else {
-                //     delete wed.commonHobbies
-                // }
-                setWork(work);
-                setGoToOccasionLastQ(false)
-                setSection("common")
-                setPage(2);
-            }}>Anterior</button>
-            <button onClick={() => {
-                if(inName !== "") {
-                    setGiftedName(inName);
-                    setWorkPage(1)
-                } else {
-                    setWarning("Por favor, preencha o nome da pessoa que vai ganhar o presente")
-                }
-            }}>Próxima</button>
-            
+                    {warning && <p className="validation-warning">{warning}</p>}
+                    
+                    <div className="prev-for">
+                        <button onClick={() => {
+                            const work = {
+                                ...futureWork,
+                                giftedName: inName
+                            }
+                            // if(inCouple) {
+                            //     delete wed.reasonToGift
+                            // } else {
+                            //     delete wed.commonHobbies
+                            // }
+                            setWork(work);
+                            setGoToOccasionLastQ(false)
+                            setSection("common")
+                            setPage(2);
+                        }}>Anterior</button>
+                        <button onClick={() => {
+                            if(inName !== "") {
+                                setGiftedName(inName);
+                                setWorkPage(1)
+                            } else {
+                                setWarning("Por favor, preencha o nome da pessoa que vai ganhar o presente")
+                            }
+                        }}>Próxima</button>
+                    </div>
+
+                </div>
+            </div>
         </div>
     )
 }
