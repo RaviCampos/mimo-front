801080d22a2b461ed7a78ce5cc0f398f036f6597
Ravi Campos <rnscampos@gmail.com>
2021-04-28 15:49:37 -0300

    lot

--- src/components/pages/questions/AdditionsQ.js
+++ src/components/pages/questions/AdditionsQ.js
@@ -10,6 +10,7 @@ function AdditionsQ({tools: {setPage, setSection, setAdditions, additions}}) {
                 <div>
                     <h2 className="small-title">Tem alguma coisa que não se encaixa em nenhuma das perguntas que eu fiz?</h2>
                     <p className="subtitle">Alguma coisa que você considera importante que eu saiba para te ajudar a escolher esse presente? Pode ser qualquer coisa, de uma história interessante a como vocês se conheceram passando pela comida preferida desta pessoa. Se não tiver nada, também não tem problema, basta deixar o campo abaixo em branco! Vamos escolher um presente bem legal mesmo assim.</p>
+                    
                     <textarea name="addition" id="addition_textarea" cols="100" rows="8" onChange={ e => setInAddtions(e.target.value)} value={inAdditions}></textarea>
 
                     <br/>
