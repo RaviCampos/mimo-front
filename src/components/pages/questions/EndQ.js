3c55a588eea704c8aa514a4959f4999347ebb8a6
Ravi Campos <rnscampos@gmail.com>
2021-05-05 19:24:42 -0300

    starting final email format

--- src/components/pages/questions/EndQ.js
+++ src/components/pages/questions/EndQ.js
@@ -2,11 +2,11 @@ import emailjs from 'emailjs-com';
 
 function EndQ({tools: {setPage, formInfo}}) {
  
-    function sendEmail() {
+    function sendEmail(message) {
         emailjs.send(
             "service_a7ydngj", 
             "template_8xqrqek", 
-            {message: JSON.stringify(formInfo, null, "\t")}, 
+            {message: message}, 
             "user_zPbvgXcdu1HcYNxPkcMOc"
         ).then(function(response) {
             console.log('SUCCESS!', response.status, response.text);
@@ -14,20 +14,34 @@ function EndQ({tools: {setPage, formInfo}}) {
             console.log('FAILED...', error);
         });
     }
+
+    let finalMessage
+
+    switch (formInfo.occasion) {
+        case "casamento/namoro":
+            finalMessage = 
+            `Nome de quem dá o presente: ${formInfo.gifterName}`
+            break;
+    
+        default:
+            finalMessage = "Algo deu errado, desculpe, você precisará refazer o formulário"
+            break;
+    }
+
     return (
         <div className="all-margin">
             <div className="all-center">
                 <div>
                     <h2>Obrigado por comprar com o mimolino</h2>            
                     <h3>Talvez revise seus dados</h3>
-                    <p>{JSON.stringify(formInfo)}</p>
+                    <p>{finalMessage}</p>
 
                     <div className="prev-for">
                         <button onClick={() => {
                             setPage(9)
                         }}>Anterior</button>
                         <button onClick={() => {
-                        sendEmail();
+                        sendEmail(JSON.stringify(formInfo, null, "\t"));
                         }}>Enviar</button>
                     </div>
                 </div>
