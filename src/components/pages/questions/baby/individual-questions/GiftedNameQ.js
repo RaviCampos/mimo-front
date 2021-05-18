21585c7d65abe6497e3f64ee4780dc85558c9894
Ravi Campos <rnscampos@gmail.com>
2021-05-18 12:14:39 -0300

    giftedName in baby

--- src/components/pages/questions/baby/individual-questions/GiftedNameQ.js
+++ src/components/pages/questions/baby/individual-questions/GiftedNameQ.js
@@ -2,35 +2,71 @@ import { useState, useEffect } from "react"
 
 function GiftedNameQ({tools: {setPage, setBabyPage, setGiftedName, giftedName, setSection, futureBaby, setBaby, setGoToOccasionLastQ}}) {
 
-    const [ inGifted, setInGifted ] = useState(giftedName ? giftedName : "");
-    const [ showWarning, setShowWarning ] = useState(false)
+    const [ who, setWho ] = useState(giftedName ? giftedName.who : "");
+    const [ name, setName ] = useState(giftedName ? giftedName.name : "");
+
+    const [ warning, setShowWarning ] = useState(false)
 
     useEffect(() => {
-        if(inGifted) {
+        if(who) {
             setShowWarning(false);
         }
-    }, [inGifted])
+    }, [who])
 
-    // giftedName = {
-    //     name: "Astoufo",
-    //     quality: "Solo"
-    // }
+    function nameInput(q) {
+        return <div>
+            <p>{q}</p>
+            <input type="text" value={name} onChange={e => setName(e.target.value)}/>
+        </div>
+    }
 
     return (
         <div className="all-margin">
             <div className="all-center">
                 <div>
-                    <h2 className="title">Uma criança é sempre uma alegria, né? Qual o nome do casal que irá recebê-la? </h2>
-                    <input className="bit-down" type="text" value={inGifted} onChange={e => setInGifted(e.target.value)} placeholder="nome" autoComplete="off"/>
+                    <h2 className="title">Uma criança é sempre uma alegria, né? Quem são os sortudos vão receber um presente seu?</h2>
 
-                    {showWarning && <p className="validation-warning">preencha o nome para seguir em frente</p>}
+                    <div>
+                        
+                        <label className="radio-option">
+                            Um casal querido que está esperando um filho, seus nomes são:
+                            <input type="radio" name="who" id="who_couple" value="Um casal querido que está esperando um filho" checked={ who === "Um casal querido que está esperando um filho"} onChange={e => {setWho(e.target.value); setName("")}}/>
+                            <span className="checkmark"></span>
+                        </label>
+                        
+                        {who === "Um casal querido que está esperando um filho" && nameInput("Seus nomes são?")}
+
+                        <label className="radio-option">
+                            Na verdade é um (a) futuro (a) Mãe / Pai Solo e seu nome é: (resposta curta)
+                            <input type="radio" name="who" id="who_solo-parent" value="Na verdade é um (a) futuro (a) Mãe / Pai Solo" checked={ who === "Na verdade é um (a) futuro (a) Mãe / Pai Solo"} onChange={e => {setWho(e.target.value); setName("")}}/>
+                            <span className="checkmark"></span>
+                        </label>
+
+                        {who === "Na verdade é um (a) futuro (a) Mãe / Pai Solo" && nameInput("Sua/seu nome é?")}
+
+                        <label className="radio-option">
+                            Na verdade, eu sou pai/mãe da criança e estou em busca de um presente para celebrarmos alegria de termos um filho
+                            <input type="radio" name="who" id="who_self-parent" value="Na verdade, eu sou pai/mãe da criança e estou em busca de um presente para celebrarmos alegria de termos um filho" checked={ who === "Na verdade, eu sou pai/mãe da criança e estou em busca de um presente para celebrarmos alegria de termos um filho"} onChange={e => {setWho(e.target.value); setName("")}}/>
+                            <span className="checkmark"></span>
+                        </label>
+
+                        {who === "Na verdade, eu sou pai/mãe da criança e estou em busca de um presente para celebrarmos alegria de termos um filho" && nameInput("Parabéns! Qual o nome da pessoa que está dividindo essa alegria com você?")}
+
+                    </div>
+
+                    {/* <input className="bit-down" type="text" value={inGifted} onChange={e => setInGifted(e.target.value)} placeholder="nome" autoComplete="off"/> */}
+
+                    {warning && <p className="validation-warning">{warning}</p>}
 
                     <br/>
                     <div className="prev-for go-down">
                         <button onClick={() => {
                             const baby = {
                                 ...futureBaby,
-                                giftedName: inGifted
+                                giftedName: {
+                                    who,
+                                    name
+                                }
                             }
                             setBaby(baby)
                             setGoToOccasionLastQ(false)
@@ -38,11 +74,16 @@ function GiftedNameQ({tools: {setPage, setBabyPage, setGiftedName, giftedName, s
                             setPage(2);
                         }}>Anterior</button>
                         <button onClick={() => {
-                            if(inGifted) {
-                                setGiftedName(inGifted)
-                                setBabyPage(1)
+                            if(!who) {
+                                setShowWarning("Por favor, escolha uma das opções acima");
+                            } else if(!name) {
+                                setShowWarning("Por favor preencha o espaço do(s) nome(s)");
                             } else {
-                                setShowWarning(true);
+                                setGiftedName({
+                                    who,
+                                    name
+                                })
+                                setBabyPage(1)
                             }
                         }}>Próxima</button>
                     </div>
