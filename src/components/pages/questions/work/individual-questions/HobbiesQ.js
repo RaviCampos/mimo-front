8d2316032d604f299851dd87261fb48ea2ca3011
Ravi Campos <rnscampos@gmail.com>
2021-06-07 12:37:07 -0300

    mood in work

--- src/components/pages/questions/work/individual-questions/HobbiesQ.js
+++ src/components/pages/questions/work/individual-questions/HobbiesQ.js
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 
 function HobbiesQ({tools: { setSection, futureWork, setWork, setPage, setGoToOccasionLastQ, setWorkPage, hobbies, setHobbies, giftedName, intimacy }}) {
 
-    const [ inHobbies, setInHobbies ] = useState(hobbies ? hobbies : "")
+    const [ inHobbies, setInHobbies ] = useState(hobbies ? hobbies.split(" [Time]: ")[0] : "")
+    const [ team, setTeam ] = useState(hobbies ? hobbies.split(" [Time]: ")[1] ? hobbies.split(" [Time]: ")[1]: "" : "")
 
     const [ warning, setWarning ] = useState(false)
     useEffect(() => {
@@ -14,25 +15,75 @@ function HobbiesQ({tools: { setSection, futureWork, setWork, setPage, setGoToOcc
             <div className="all-center">
                 <div>
                     <h2 className="title small-title">Não é só de trabalho que se vive, não é mesmo? Qual das opções abaixo combina mais com o que {giftedName} faria em um final de semana?</h2>
-                    <input type="text" placeholder="área" value={inHobbies} onChange={e => setInHobbies(e.target.value)} autoComplete="off"/>
-
-                    <br/>
+                    
+                    <div>
+                        <label className="radio-option small-radio long-option">
+                            {giftedName} gosta de tirar um tempo para si, uma yoga, uma meditação, cuidar de suas plantas, essas coisas, sabe?
+                            <input type="radio" name="hobbies" id="hobbies_easy" checked={inHobbies === `${giftedName} gosta de tirar um tempo para si, uma yoga, uma meditação, cuidar de suas plantas, essas coisas, sabe?`} onChange={() => setInHobbies(`${giftedName} gosta de tirar um tempo para si, uma yoga, uma meditação, cuidar de suas plantas, essas coisas, sabe?`)} />
+                            <span className="checkmark"></span>
+                        </label>
+                        <label className="radio-option small-radio long-option">
+                            Onde tiver uma festa, é lá que {giftedName} estará
+                            <input type="radio" name="hobbies" id="hobbies_party" checked={inHobbies === `Onde tiver uma festa, é lá que ${giftedName} estará`} onChange={() => setInHobbies(`Onde tiver uma festa, é lá que ${giftedName} estará`)} />
+                            <span className="checkmark"></span>
+                        </label>
+                        <label className="radio-option small-radio long-option">
+                            Colocar as séries e os filmes em dia, com certeza
+                            <input type="radio" name="hobbies" id="hobbies_films" checked={inHobbies === `Colocar as séries e os filmes em dia, com certeza`} onChange={() => setInHobbies(`Colocar as séries e os filmes em dia, com certeza`)} />
+                            <span className="checkmark"></span>
+                        </label>
+                        <label className="radio-option small-radio long-option">
+                            Apreciar um bom vinho e uma boa comida, esse é um programa que combina com {giftedName}
+                            <input type="radio" name="hobbies" id="hobbies_wine" checked={inHobbies === `Apreciar um bom vinho e uma boa comida, esse é um programa que combina com ${giftedName}`} onChange={() => setInHobbies(`Apreciar um bom vinho e uma boa comida, esse é um programa que combina com ${giftedName}`)} />
+                            <span className="checkmark"></span>
+                        </label>
+                        <label className="radio-option small-radio long-option">
+                            Finais de semana são para a família e para a cozinha, duas coisas que {giftedName} adora
+                            <input type="radio" name="hobbies" id="hobbies_wine" checked={inHobbies === `Finais de semana são para a família e para a cozinha, duas coisas que ${giftedName} adora`} onChange={() => setInHobbies(`Finais de semana são para a família e para a cozinha, duas coisas que ${giftedName} adora`)} />
+                            <span className="checkmark"></span>
+                        </label>
+                        <label className="radio-option small-radio long-option">
+                            Final de semana tem jogo, não é mesmo? Então é isso que {giftedName} estará fazendo, assistindo e torcendo
+                            <input type="radio" name="hobbies" id="hobbies_game" checked={inHobbies === `Final de semana tem jogo, não é mesmo? Então é isso que ${giftedName} estará fazendo, assistindo e torcendo`} onChange={() => setInHobbies(`Final de semana tem jogo, não é mesmo? Então é isso que ${giftedName} estará fazendo, assistindo e torcendo`)} />
+                            <span className="checkmark"></span>
+                        </label>
+                        {inHobbies === `Final de semana tem jogo, não é mesmo? Então é isso que ${giftedName} estará fazendo, assistindo e torcendo` && <div>
+                            <p>Qual o esporte e qual time?</p>
+                            <input type="text" placeholder="time" value={team} onChange={e => setTeam(e.target.value)}/>
+                        </div>}
+                        <label className="radio-option small-radio long-option">
+                            “Não é só de trabalho que se vive, não é mesmo?” {giftedName} com certeza nunca ouviu isso, final de semana é só um dia normal com uma pausa maior para o almoço
+                            <input type="radio" name="hobbies" id="hobbies_work" checked={inHobbies === `“Não é só de trabalho que se vive, não é mesmo?” ${giftedName} com certeza nunca ouviu isso, final de semana é só um dia normal com uma pausa maior para o almoço`} onChange={() => setInHobbies(`“Não é só de trabalho que se vive, não é mesmo?” ${giftedName} com certeza nunca ouviu isso, final de semana é só um dia normal com uma pausa maior para o almoço`)} />
+                            <span className="checkmark"></span>
+                        </label>
+                    </div>
 
                     {warning && <p className="validation-warning">{warning}</p>}
                     
                     <div className="prev-for">
                         <button onClick={() => {
-                            setHobbies(inHobbies);
+                            let finalHobbies
+                            if(inHobbies === `Final de semana tem jogo, não é mesmo? Então é isso que ${giftedName} estará fazendo, assistindo e torcendo`) {
+                                finalHobbies = inHobbies + " [Time]: " + team;
+                            } else {
+                                finalHobbies = inHobbies;
+                            }
+                            setHobbies(finalHobbies);
                             setWorkPage(intimacy ? 6 : 4)
                         }}>Anterior</button>
                         <button onClick={() => {
                             if(!inHobbies) {
                                 setWarning("Por favor, preencha a área")
                             } else {
-                                setHobbies(inHobbies);
+                                let finalHobbies
+                                if(inHobbies === `Final de semana tem jogo, não é mesmo? Então é isso que ${giftedName} estará fazendo, assistindo e torcendo`) {
+                                    finalHobbies = inHobbies + " [Time]: " + team;
+                                } else {
+                                    finalHobbies = inHobbies;
+                                }
                                 const work = {
                                     ...futureWork,
-                                    hobbies: inHobbies
+                                    hobbies: finalHobbies
                                 }
                                 setWork(work);
                                 setGoToOccasionLastQ(true)
--- src/components/pages/questions/work/individual-questions/MoodQ.js
+++ src/components/pages/questions/work/individual-questions/MoodQ.js
@@ -9,20 +9,45 @@ function MoodQ({tools: { setWorkPage, mood, setMood, giftedName }}) {
     }, [inMood])
 
     return (
-        <div>
+        <div className="all-margin">
+            <div className="all-center">
+                <div>
+                    <h2 className="title small-title">Você que conhece {giftedName}, como acha que ele(a) está se sentido agora com essa mudança?</h2>
+                    
+                    <div>
+                        <label className="radio-option long-option">
+                            Com certeza bastante animado(a), não para de falar nisso e parece estar bastante feliz
+                            <input type="radio" name="mood" id="mood_nonstop" checked={inMood === `Com certeza bastante animado(a), não para de falar nisso e parece estar bastante feliz`} onChange={() => setInMood(`Com certeza bastante animado(a), não para de falar nisso e parece estar bastante feliz`)} />
+                            <span className="checkmark"></span>
+                        </label>
+                        <label className="radio-option long-option">
+                            Uma mudança mesmo que boa sempre provoca um frio na barriga, não é mesmo? Mas {giftedName} está preparado(a) e merece essa oportunidade
+                            <input type="radio" name="mood" id="mood_party" checked={inMood === `Uma mudança mesmo que boa sempre provoca um frio na barriga, não é mesmo? Mas ${giftedName} está preparado(a) e merece essa oportunidade`} onChange={() => setInMood(`Uma mudança mesmo que boa sempre provoca um frio na barriga, não é mesmo? Mas ${giftedName} está preparado(a) e merece essa oportunidade`)} />
+                            <span className="checkmark"></span>
+                        </label>
+                        <label className="radio-option long-option">
+                            Acho que {giftedName} está um pouco nervoso, confesso que eu também ficaria, mas {giftedName} tem todo o meu apoio e espero que ele(a) saiba disso
+                            <input type="radio" name="mood" id="mood_films" checked={inMood === `Acho que ${giftedName} está um pouco nervoso, confesso que eu também ficaria, mas ${giftedName} tem todo o meu apoio e espero que ele(a) saiba disso`} onChange={() => setInMood(`Acho que ${giftedName} está um pouco nervoso, confesso que eu também ficaria, mas ${giftedName} tem todo o meu apoio e espero que ele(a) saiba disso`)} />
+                            <span className="checkmark"></span>
+                        </label>
+                    </div>
 
-            <h2>Ânimo</h2>
+                    {warning && <p className="validation-warning">{warning}</p>}
+                    
+                    <div className="prev-for">
+                        <button onClick={() => {
+                            setMood(inMood);
+                            setWorkPage(4)
+                        }}>Anterior</button>
+                        <button onClick={() => {
+                            if(!inMood) return setWarning("Por favor, escolha uma das opções")
+                            setMood(inMood);
+                            setWorkPage(6)
+                        }}>Próxima</button>
+                    </div>
 
-            {warning && <p className="validation-warning">{warning}</p>}
-
-            <button onClick={() => {
-                setMood(inMood);
-                setWorkPage(4)
-            }}>Anterior</button>
-            <button onClick={() => {
-                setMood(inMood);
-                setWorkPage(6)
-            }}>Próxima</button>
+                </div>
+            </div>
         </div>
     )
 }
