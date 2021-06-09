d16d6535e0d20945f7f72ebf3d4a730a117c94f1
Ravi Campos <rnscampos@gmail.com>
2021-06-08 21:58:04 -0300

    coolness and mood in moving

--- src/components/pages/questions/moving/individual-questions/CoolnessQ.js
+++ src/components/pages/questions/moving/individual-questions/CoolnessQ.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-function CoolnessQ({tools: { setSection, futureMoving, setMoving, setPage, setGoToOccasionLastQ, setMovingPage, coolness, setCoolness, giftedName, intimacy }}) {
+function CoolnessQ({tools: { setMovingPage, coolness, setCoolness, giftedName, intimacy }}) {
     const [ inCoolness, setInCoolness ] = useState(coolness)
 
     const [ warning, setWarning ] = useState(false)
@@ -9,20 +9,51 @@ function CoolnessQ({tools: { setSection, futureMoving, setMoving, setPage, setGo
     }, [inCoolness])
 
     return (
-        <div>
+        <div className="all-margin">
+            <div className="all-center">
+                <div>
+                    <h2 className="small-title">Uma mudança exige muita preparação, qual você acredita que sejam as prioridades de {giftedName} nesse processo?</h2>
+                    <div>
+                        <label className="radio-option long-option">
+                            Um colchão, um cooler e a internet instalada já estaria ótimo para {giftedName}, o que importa é ter conquistado esse espaço para si
+                            <input type="radio" name="coolness" id="coolness_cooler" checked={inCoolness === `Um colchão, um cooler e a internet instalada já estaria ótimo para ${giftedName}, o que importa é ter conquistado esse espaço para si`} onChange={() => setInCoolness(`Um colchão, um cooler e a internet instalada já estaria ótimo para ${giftedName}, o que importa é ter conquistado esse espaço para si`)} />
+                            <span className="checkmark"></span>
+                        </label>
+                        <label className="radio-option long-option">
+                            Se as pastas no Pinterest e todos os instagram de decoração de {giftedName} vem seguindo dizem alguma coisa é que esse lugar vai ter a sua cara e isso é muito importante para ele(a)
+                            <input type="radio" name="coolness" id="coolness_pinterest" checked={inCoolness === `Se as pastas no Pinterest e todos os instagram de decoração de ${giftedName} vem seguindo dizem alguma coisa é que esse lugar vai ter a sua cara e isso é muito importante para ele(a)`} onChange={() => setInCoolness(`Se as pastas no Pinterest e todos os instagram de decoração de ${giftedName} vem seguindo dizem alguma coisa é que esse lugar vai ter a sua cara e isso é muito importante para ele(a)`)} />
+                            <span className="checkmark"></span>
+                        </label>
+                        <label className="radio-option long-option">
+                            A cozinha com certeza será o ambiente sagrado, talvez até o primeiro a ser arrumado. {giftedName} adora cozinhar e deve estar ansioso(a) para ter esse espaço novo para si
+                            <input type="radio" name="coolness" id="coolness_kitchen" checked={inCoolness === `A cozinha com certeza será o ambiente sagrado, talvez até o primeiro a ser arrumado. ${giftedName} adora cozinhar e deve estar ansioso(a) para ter esse espaço novo para si`} onChange={() => setInCoolness(`A cozinha com certeza será o ambiente sagrado, talvez até o primeiro a ser arrumado. ${giftedName} adora cozinhar e deve estar ansioso(a) para ter esse espaço novo para si`)} />
+                            <span className="checkmark"></span>
+                        </label>
+                        <label className="radio-option long-option long-option">
+                            {giftedName} é muito pragmático e não vai descansar enquanto não estiver tudo no lugar
+                            <input type="radio" name="coolness" id="coolness_norest" checked={inCoolness === `${giftedName} é muito pragmático e não vai descansar enquanto não estiver tudo no lugar`} onChange={() =>setInCoolness(`${giftedName} é muito pragmático e não vai descansar enquanto não estiver tudo no lugar`)} />
+                            <span className="checkmark"></span>
+                        </label>
+                    </div>
 
-            <h2>Careta ou descolado</h2>
+                    {warning && <p className="validation-warning">{warning}</p>}
 
-            {warning && <p className="validation-warning">{warning}</p>}
-
-            <button onClick={() => {
-                setCoolness(inCoolness);
-                setMovingPage(intimacy > 5 ? 5 : 4)
-            }}>Anterior</button>
-            <button onClick={() => {
-                setCoolness(inCoolness);
-                setMovingPage(intimacy > 5 ? 7 : 6)
-            }}>Próxima</button>
+                    <div className="prev-for go-bit-down when-mobile">
+                        <button onClick={() => {
+                            setCoolness(inCoolness);
+                            setMovingPage(intimacy > 5 ? 5 : 4);
+                        }}>Anterior</button>
+                        <button onClick={() => {
+                            if(inCoolness) {
+                                setCoolness(inCoolness);
+                                setMovingPage(intimacy > 5 ? 7 : 6);
+                            } else {
+                                setWarning("Por favor, escolha uma das opções")
+                            }
+                        }}>Próxima</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/pages/questions/moving/individual-questions/MoodQ.js
+++ src/components/pages/questions/moving/individual-questions/MoodQ.js
@@ -9,20 +9,51 @@ function MoodQ({tools: { setMovingPage, mood, setMood, giftedName}}) {
     }, [inMood])
 
     return (
-        <div>
+        <div className="all-margin">
+            <div className="all-center">
+                <div>
+                    <h2 className="small-title">Uma mudança mesmo que boa sempre provoca um frio na barriga, não é mesmo? Como você acha que {giftedName} está se sentido com relação a isso agora?</h2>
+                    <div>
+                        <label className="radio-option long-option">
+                            Com certeza bastante animado(a), não para de falar nisso e parece estar bastante feliz
+                            <input type="radio" name="mood" id="mood_happy" checked={inMood === `Com certeza bastante animado(a), não para de falar nisso e parece estar bastante feliz`} onChange={() => setInMood(`Com certeza bastante animado(a), não para de falar nisso e parece estar bastante feliz`)} />
+                            <span className="checkmark"></span>
+                        </label>
+                        <label className="radio-option long-option">
+                            Com ou sem frio na barriga {giftedName} está pronto(a) para essa mudança, tanto psicologicamente quanto prática mente, nunca vi caixa tão organizadas e bem etiquetadas
+                            <input type="radio" name="mood" id="mood_organized" checked={inMood === `Com ou sem frio na barriga ${giftedName} está pronto(a) para essa mudança, tanto psicologicamente quanto prática mente, nunca vi caixa tão organizadas e bem etiquetadas`} onChange={() => setInMood(`Com ou sem frio na barriga ${giftedName} está pronto(a) para essa mudança, tanto psicologicamente quanto prática mente, nunca vi caixa tão organizadas e bem etiquetadas`)} />
+                            <span className="checkmark"></span>
+                        </label>
+                        <label className="radio-option long-option">
+                            Acho que {giftedName} está um pouco nervoso, confesso que eu também ficaria, mas {giftedName} tem todo o meu apoio e espero que ele(a) saiba disso
+                            <input type="radio" name="mood" id="mood_nervous" checked={inMood === `Acho que ${giftedName} está um pouco nervoso, confesso que eu também ficaria, mas ${giftedName} tem todo o meu apoio e espero que ele(a) saiba disso`} onChange={() => setInMood(`Acho que ${giftedName} está um pouco nervoso, confesso que eu também ficaria, mas ${giftedName} tem todo o meu apoio e espero que ele(a) saiba disso`)} />
+                            <span className="checkmark"></span>
+                        </label>
+                        <label className="radio-option long-option long-option">
+                            Difícil dizer, {giftedName} é muito discreto(a). Mas como diz na pergunta, uma mudança sempre provoca um frio na barriga
+                            <input type="radio" name="mood" id="mood_shy" checked={inMood === `Difícil dizer, ${giftedName} é muito discreto(a). Mas como diz na pergunta, uma mudança sempre provoca um frio na barriga`} onChange={() =>setInMood(`Difícil dizer, ${giftedName} é muito discreto(a). Mas como diz na pergunta, uma mudança sempre provoca um frio na barriga`)} />
+                            <span className="checkmark"></span>
+                        </label>
+                    </div>
 
-            <h2>Ânimo</h2>
+                    {warning && <p className="validation-warning">{warning}</p>}
 
-            {warning && <p className="validation-warning">{warning}</p>}
-
-            <button onClick={() => {
-                setMood(inMood);
-                setMovingPage(4)
-            }}>Anterior</button>
-            <button onClick={() => {
-                setMood(inMood);
-                setMovingPage(6)
-            }}>Próxima</button>
+                    <div className="prev-for go-bit-down when-mobile">
+                        <button onClick={() => {
+                            setMood(inMood);
+                            setMovingPage(4)
+                        }}>Anterior</button>
+                        <button onClick={() => {
+                            if(inMood) {
+                                setMood(inMood);
+                                setMovingPage(6)
+                            } else {
+                                setWarning("Por favor, escolha uma das opções")
+                            }
+                        }}>Próxima</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
