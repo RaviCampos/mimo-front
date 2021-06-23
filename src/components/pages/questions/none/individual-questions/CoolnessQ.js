40970d99683307125df36fb307a24f1799168702
Ravi Campos <rnscampos@gmail.com>
2021-06-23 15:50:27 -0300

    fixed coolnessQ in none section

--- src/components/pages/questions/none/individual-questions/CoolnessQ.js
+++ src/components/pages/questions/none/individual-questions/CoolnessQ.js
@@ -18,53 +18,66 @@ function CoolnessQ({tools: { setSection, futureNone, setNone, setPage, setGoToOc
     if(intimacy > 5) {
 
         return (
-            <div>
-    
-                <h2>Tem algum filme ou série que te lembra bastante essa pessoa? Se você quiser, pode me contar o porquê?</h2>
-                <textarea value={inFilms} onChange={e => setInFilms(e.target.value)}/>
+            <div className="all-margin">
+                <div className="all-center">
+                    <div>
+                        <h2>Tem algum filme ou série que te lembra bastante essa pessoa? Se você quiser, pode me contar o porquê?</h2>
+                        <p className="subtitle">Você pode deixar essa resposta em branco se preferir</p>
+                        <textarea value={inFilms} onChange={e => setInFilms(e.target.value)}/>
 
-                <h2>E música? Tem alguma música, cantor ou banda que sempre que toca te lembra dessa pessoa? Se você quiser, pode me contar o porquê?</h2>
-                <textarea value={inMusics} onChange={e => setInMusics(e.target.value)}/>
-    
-                <button onClick={() => {
-                    setFilms(inFilms);
-                    setMusics(inMusics);
-                    setNonePage(4)
-                }}>Anterior</button>
-                <button onClick={() => {
-                    setFilms(inFilms);
-                    setMusics(inMusics);
-                    setNonePage(6)
-                }}>Próxima</button>
+                        <h2>E música? Tem alguma música, cantor ou banda que sempre que toca te lembra dessa pessoa? Se você quiser, pode me contar o porquê?</h2>
+                        <p className="subtitle">Você pode deixar essa resposta em branco se preferir</p>
+                        <textarea value={inMusics} onChange={e => setInMusics(e.target.value)}/>
+            
+                        <div className="prev-for">
+                            <button onClick={() => {
+                                setFilms(inFilms);
+                                setMusics(inMusics);
+                                setNonePage(4)
+                            }}>Anterior</button>
+                            <button onClick={() => {
+                                setFilms(inFilms);
+                                setMusics(inMusics);
+                                setNonePage(6)
+                            }}>Próxima</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
 
     } else {
 
         return (
-            <div>
-    
-                <h2>Careta ou descolado</h2>
-    
-                {warning && <p className="validation-warning">{warning}</p>}
-    
-                <button onClick={() => {
-                    setCoolness(inCoolness);
-                    setNonePage(4)
-                }}>Anterior</button>
-                <button onClick={() => {
-                    const none = {
-                        ...futureNone,
-                        coolness: inCoolness
-                    }
-                    delete none.hobbies
-                    delete none.films
-                    delete none.musics
-                    setNone(none);
-                    setGoToOccasionLastQ(true)
-                    setSection("common")
-                    setPage(4);
-                }}>Próxima</button>
+            <div className="all-margin">
+                <div className="all-center">
+                    <div>
+            
+                        <h2>Careta ou descolado</h2>
+            
+                        {warning && <p className="validation-warning">{warning}</p>}
+            
+                        <div className="prev-for">
+                            <button onClick={() => {
+                                setCoolness(inCoolness);
+                                setNonePage(4)
+                            }}>Anterior</button>
+                            <button onClick={() => {
+                                const none = {
+                                    ...futureNone,
+                                    coolness: inCoolness
+                                }
+                                delete none.hobbies
+                                delete none.films
+                                delete none.musics
+                                setNone(none);
+                                setGoToOccasionLastQ(true)
+                                setSection("common")
+                                setPage(4);
+                            }}>Próxima</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
