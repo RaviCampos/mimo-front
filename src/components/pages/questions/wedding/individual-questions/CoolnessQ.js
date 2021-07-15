4359afad3aca2725c4e3331f2d2488813a29b7b6
Ravi Campos <rnscampos@gmail.com>
2021-07-15 08:49:04 -0300

    did

--- src/components/pages/questions/wedding/individual-questions/CoolnessQ.js
+++ src/components/pages/questions/wedding/individual-questions/CoolnessQ.js
@@ -36,6 +36,7 @@ const baseState = (giftedName) => ({
     }
 })
 
+
 function CoolnessQ({tools: { setWeddingPage, setCoolness, gifterInCouple, coolness, giftedName, setSection, futureWedding, setWedding, setPage, setGoToOccasionLastQ  }}) {
     const [ inCoolnessCheckbox, dispatch ] = useReducer(reducer, {prevStateFromFatherComponent: coolness, baseState: baseState(giftedName)}, init)
     
